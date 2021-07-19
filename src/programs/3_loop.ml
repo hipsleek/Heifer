09b225b8118ddaa2806c82e35b21185d29241f75
songyahui <songyahuiapp@hotmail.com>
2021-07-19 12:06:00 +0800

    m

--- src/programs/3_loop.ml
+++ src/programs/3_loop.ml
@@ -1,28 +1,21 @@
-effect Foo : (int -> int)
+effect Foo : (unit -> int)
 
 effect Goo : (unit -> int)
 
-effect Zoo : (unit -> int)
-
 let f () 
 (*@ requires emp @*)
-(*@ ensures  Foo(1) @*)
-  = perform Foo 1
-
+(*@ ensures  Foo @*)
+  = perform Foo ()
 
 let res : int
   (*@ requires emp @*)
-  (*@ ensures  (Foo (_).Goo ().Foo (100))^w @*)
-  Foo(1).Goo().(Foo(100).Goo())^w
+  (*@ ensures  (Foo.Goo)^w @*)
   =
   match f () with
   | x -> 1
   | effect Foo k ->
-     continue k (fun x -> perform Goo ())
-// Foo (x) -> Goo ()
-  | effect Goo k ->
-     continue k (fun () -> perform Foo 100)
-// Goo () -> Foo 100
+     continue k (fun () -> perform Goo ())
 
-1. parser 
-2. infer 
+  | effect Goo k ->
+     continue k (fun () -> perform Foo ())
+
