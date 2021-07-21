441909e428f1f55755c74465393e3db0159c1b48
songyahui <songyahuiapp@hotmail.com>
2021-07-21 21:08:54 +0800

    m

---
+++ parsing/hip.ml
@@ -0,0 +1,194 @@
+
+
+exception Foo of string
+open Parsetree
+open Asttypes
+
+
+
+let rec input_lines file =
+  match try [input_line file] with End_of_file -> [] with
+   [] -> []
+  | [line] -> (String.trim line) :: input_lines file
+  | _ -> failwith "Weird input_line return value"
+;;
+
+
+
+
+
+
+(*
+let string_of_effect_constructor x :string =
+  match x.peff_kind with
+  | Peff_decl(_, _) -> ""
+      
+  | _ -> ""
+;;
+type rec_flag = Nonrecursive | Recursive
+*)
+
+let string_of_payload _ = "payload\n";;
+
+let string_of_attribute (a:attribute) : string = 
+  let name = a.attr_name.txt in 
+  let payload = a.attr_payload in 
+  name ^ string_of_payload payload ;;
+
+let string_of_attributes (a_l:attributes): string = 
+  List.fold_left (fun acc a -> acc ^ string_of_attribute a ) "" a_l;;
+
+let string_of_arg_label a = 
+  match a with 
+  | Nolabel -> ""
+  | Labelled str -> str (*  label:T -> ... *)
+  | Optional str -> "?" ^ str (* ?label:T -> ... *)
+
+;;
+
+let rec string_of_core_type (p:core_type) :string =
+  match p.ptyp_desc with 
+  | Ptyp_any -> "_"
+  | Ptyp_var str -> str
+  | Ptyp_arrow (a, c1, c2) -> string_of_arg_label a ^  string_of_core_type c1 ^ " -> " ^ string_of_core_type c2 ^"\n"
+  | Ptyp_constr (l, c_li) -> 
+    List.fold_left (fun acc a -> acc ^ a) "" (Longident.flatten l.txt)^
+    List.fold_left (fun acc a -> acc ^ string_of_core_type a) "" c_li
+  | Ptyp_poly (str_li, c) -> 
+    "type " ^ List.fold_left (fun acc a -> acc ^ a.txt) "" str_li ^ ". " ^
+    string_of_core_type c 
+
+
+  | _ -> "\nlsllsls\n"
+  ;;
+
+
+let string_of_kind k : string = 
+  match k with 
+  | Peff_decl (inp,outp)-> 
+    List.fold_left (fun acc a -> acc ^ string_of_core_type a) "" inp 
+    ^
+    string_of_core_type outp
+    
+  | Peff_rebind _ -> "Peff_rebind"
+  ;;
+
+let string_of_p_constant con : string =
+  match con with 
+  | Pconst_char i -> String.make 1 i
+  | Pconst_string (i, _, None) -> i
+  | Pconst_string (i, _, Some delim) -> i ^ delim
+  | Pconst_integer (i, None) -> i
+  | _ -> "string_of_p_constant"
+;;
+
+(*
+
+  | Pconst_integer (i, Some m) ->
+  paren (first_is '-' i) (fun f (i, m) -> pp f "%s%c" i m) f (i,m)
+  | Pconst_float (i, None) ->
+  paren (first_is '-' i) (fun f -> pp f "%s") f i
+  | Pconst_float (i, Some m) ->
+  paren (first_is '-' i) (fun f (i,m) -> pp f "%s%c" i m) f (i,m)
+  *)
+
+let rec string_of_pattern (p) : string = 
+  match p.ppat_desc with 
+  | Ppat_any -> "_"
+  (* _ *)
+  | Ppat_var str -> str.txt
+  (* x *)
+  | Ppat_constant con -> string_of_p_constant con
+  | Ppat_type l -> List.fold_left (fun acc a -> acc ^ a) "" (Longident.flatten l.txt)
+  | Ppat_constraint (p1, c) -> string_of_pattern p1 ^ " : "^ string_of_core_type c
+  (* (P : T) *)
+  | Ppat_effect (p1, p2) -> string_of_pattern p1 ^ string_of_pattern p2 ^ "\n"
+
+  | Ppat_construct (l, None) -> List.fold_left (fun acc a -> acc ^ a) "" (Longident.flatten l.txt)
+  | Ppat_construct (l, Some p1) ->  
+    List.fold_left (fun acc a -> acc ^ a) "" (Longident.flatten l.txt) ^ 
+    string_of_pattern p1
+  (* #tconst *)
+
+  | _ -> "string_of_pattern\n" ;;
+
+
+
+
+let rec string_of_value_binding vb : string = 
+  let rec string_of_expression e: string =
+    match e.pexp_desc with 
+    | Pexp_ident l -> List.fold_left (fun acc a -> acc ^ a) "" (Longident.flatten l.txt)
+    | Pexp_constant con  -> string_of_p_constant con 
+    | Pexp_let (_, vb_li, e1) -> 
+      List.fold_left (fun acc a -> acc ^ string_of_value_binding a) "" vb_li ^ 
+      string_of_expression e1
+
+    | Pexp_fun (al, None, p1, e1) -> 
+      string_of_arg_label al ^ 
+      string_of_pattern p1 ^
+      string_of_expression e1 
+
+    | Pexp_fun (al, Some e0, p1, e1) -> 
+      string_of_arg_label al ^ 
+      string_of_expression e0 ^ 
+      string_of_pattern p1 ^
+      string_of_expression e1 
+
+    | Pexp_match _ -> "\nPexp_match\n"
+  
+  
+    | _ -> "\nstring_of_expression"
+  in 
+  let pattern = vb.pvb_pat in 
+  let expression = vb.pvb_expr in
+  let attributes = vb.pvb_attributes in 
+  string_of_pattern pattern ^ " = " ^ 
+  string_of_expression expression ^  "\n" ^
+  string_of_attributes attributes ^ "\n"
+
+
+
+  ;;
+
+
+
+let forward x : string= 
+  match x.pstr_desc with
+  | Pstr_value (_, l) ->
+    List.fold_left (fun acc a -> acc ^ string_of_value_binding a) "" l
+     
+  | Pstr_effect ed -> 
+    let name = ed.peff_name.txt in 
+    let kind = ed.peff_kind in 
+    (name^ " : " ^ string_of_kind kind)
+  | _ ->  ("empty")
+  ;;
+
+  
+
+
+
+
+
+let () =
+  let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in
+(*    let outputfile = (Sys.getcwd ()^ "/" ^ Sys.argv.(2)) in
+print_string (inputfile ^ "\n" ^ outputfile^"\n");*)
+  let ic = open_in inputfile in
+  try
+      let lines =  (input_lines ic ) in
+      let line = List.fold_right (fun x acc -> acc ^ "\n" ^ x) (List.rev lines) "" in
+      let progs = Parser.implementation Lexer.token (Lexing.from_string line) in
+      
+      (*print_string (Pprintast.string_of_structure progs ) ; *)
+      print_string (List.fold_left (fun acc a -> acc ^ forward a) "" progs);
+      flush stdout;                (* 现在写入默认设备 *)
+      close_in ic                  (* 关闭输入通道 *)
+
+    with e ->                      (* 一些不可预见的异常发生 *)
+      close_in_noerr ic;           (* 紧急关闭 *)
+      raise e                      (* 以出错的形式退出: 文件已关闭,但通道没有写入东西 *)
+
+   ;;
+
