ec403c78666c7c24250897c5c42c7f99d3119776
Darius Foo <darius.foo.tw@gmail.com>
2021-08-21 01:08:33 +0800

    Motivating example

---
+++ src/programs/e1_transaction.ml
@@ -0,0 +1,70 @@
+
+type bottom
+
+module type TXN = sig
+  type 'a t
+  val atomically : (unit -> unit) -> unit
+  val ref : 'a -> 'a t
+  val (!) : 'a t -> 'a
+  val (:=) : 'a t -> 'a -> unit
+end
+
+module Txn : TXN = struct
+  type 'a t = 'a ref
+
+  effect Update : 'a t * 'a -> unit
+
+  let atomically f =
+  (* requires emp /\ Eff(f()) = U^*.(Res \/ emp)
+     ensures  U^*.(Res \/ emp) *)
+
+
+    let comp =
+      match f () with
+      | x -> (fun _ -> x)
+      | exception e -> (fun rb -> rb (); raise e)
+      | effect (Update (r,v)) k -> (fun rb ->
+          let old_v = !r in
+          r := v;
+          continue k () (fun () -> r := old_v; rb ()))
+    in comp (fun () -> ())
+
+  let ref = ref
+  let (!) = (!)
+  let (:=) r v
+(* requires emp
+   ensures  Update.Q(Update(r,v)) *)
+  = perform (Update (r,v))
+end
+
+open Txn
+
+let account1 = ref 10
+let account2 = ref 0
+
+let transfer n =
+  atomically (fun () ->
+    Format.printf "attempting to transfer %d... @?" n;
+    account1 := !account1 - n;
+    account2 := !account2 + n;
+
+    if !account1 < 0 then
+      failwith "insufficient balance!"
+    else
+      print_endline "success!")
+
+let print_balance () =
+  Format.printf "account1: %d\naccount2: %d@." !account1 !account2
+
+let move_money () =
+  print_balance ();
+  begin try
+    transfer 9;
+    transfer 4;
+  with Failure s ->
+    print_endline s
+  end;
+  print_balance ()
+
+let () =
+  move_money ()
