7e63103dd0166018a439261873461fe63c29b1dc
Darius Foo <darius.foo.tw@gmail.com>
2021-10-13 13:51:52 +0800

    Clean up example

--- src/programs.t/fixpoint_infinite5.ml
+++ src/programs.t/fixpoint_infinite5.ml
@@ -3,35 +3,41 @@ effect Ready: unit
 effect Wait : unit
 effect Done : (unit -> unit)
 
-let rec until_ready () 
-(*@ requires (_^* ) @*)
-(*@ ensures (Wait^* ) . Ready @*)
-= if true then perform Ready
-   else (perform Wait;
-      until_ready ())
-    
- let rec send_msgs n 
-(*@ requires (_^* )  @*)
-(*@ ensures (Send^* ) @*)
- = if n = 0 then ()
-   else (perform Send; 
-       send_msgs (n-1))
+let rec until_ready ()
+  (*@ requires _^* @*)
+  (*@ ensures Wait^* . Ready @*)
+=
+  if nondet_bool () then
+    perform Ready
+  else
+    (perform Wait;
+     until_ready ())
 
- let rec messenger n 
-  (*@ requires (_^* ) @*)
-  (*@ ensures (((Wait^* ) . Ready).(Send^* )).Done. Q(Done ()) @*)
- = until_ready ();
+let rec send_msgs n
+   (*@ requires _^* @*)
+   (*@ ensures Send^* @*)
+=
+  if n = 0 then ()
+  else
+    (perform Send;
+     send_msgs (n-1))
+
+let messenger n
+  (*@ requires _^* @*)
+  (*@ ensures Wait^* . Ready. Send^* . Done. Q(Done ()) @*)
+=
+   until_ready ();
    send_msgs n;
-   perform Done ()
+   let g = perform Done in
+   g ()
 
-let main 
+let main ()
    (*@ requires emp @*)
-   (*@ ensures (_)^w @*)
-= 
-   match (messenger 3) with 
-   | _ -> ()
-   | effect Ready k -> continue k ()
-   | effect Send k -> continue k ()
-   | effect Wait k -> continue k ()
-   | effect Done k -> 
-      continue k (fun _ -> messenger 3)
+   (*@ ensures (Wait^* . Ready . Send^* . Done)^w @*)
+=
+  match (messenger 3) with
+  | _ -> ()
+  | effect Ready k | effect Send k | effect Wait k ->
+    continue k ()
+  | effect Done k ->
+    continue k (fun _ -> messenger 3)
