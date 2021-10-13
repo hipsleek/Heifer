f775131755542d093fbc09715246ae5d43d48448
Darius Foo <darius.foo.tw@gmail.com>
2021-10-14 00:45:02 +0800

    Add edge case to file example

--- src/programs.t/files.ml
+++ src/programs.t/files.ml
@@ -3,18 +3,18 @@ effect Open : int
 effect Close : int -> unit
 
 let open_file ()
-(*@ requires _^* @*)
-(*@ ensures Open @*)
+  (*@ requires _^* @*)
+  (*@ ensures Open @*)
 = perform Open
 
 let close_file s
-(*@ requires (_^* ) .Open . ((~Close)^*) @*)
-(*@ ensures Close @*)
+  (*@ requires (_^* ) .Open . ((~Close)^*) @*)
+  (*@ ensures Close @*)
 = perform (Close s)
 
 let file ()
-(*@ requires emp @*)
-(*@ ensures Open.Close @*)
+  (*@ requires emp @*)
+  (*@ ensures Open.Close @*)
 = let fd = open_file () in
   close_file fd
 
@@ -29,4 +29,35 @@ let main ()
   | effect Open k ->
     continue k 1
 
+let file1 ()
+  (*@ requires emp @*)
+  (*@ ensures _^* @*)
+= let fd = open_file () in
+  let fd = open_file () in
+  close_file fd
 
+let file2 ()
+  (*@ requires emp @*)
+  (*@ ensures _^* @*)
+= let fd = open_file () in
+  close_file fd
+  (* fails as expected *)
+  (* ; close_file fd *)
+
+let file3 ()
+  (*@ requires emp @*)
+  (*@ ensures _^* @*)
+= let fd = open_file () in
+  let fd = open_file () in
+  close_file fd
+  (* fails? *)
+  (* ; close_file fd *)
+
+(* but this is ok *)
+let file4 ()
+  (*@ requires emp @*)
+  (*@ ensures _^* @*)
+= let fd = open_file () in
+  close_file fd;
+  let fd = open_file () in
+  close_file fd
