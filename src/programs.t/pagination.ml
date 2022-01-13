141ea28f3f8b8670cfac17fb934c9909901be80b
Darius Foo <darius.foo.tw@gmail.com>
2022-01-13 19:53:00 +0800

    Change names

--- src/programs.t/pagination.ml
+++ src/programs.t/pagination.ml
@@ -2,10 +2,10 @@
 let sublist low high list =
    List.filteri (fun i _ -> i >= low && i < high) list
 
-type 'a res = Res of 'a * (unit -> 'a res)
+type 'a page = Page of 'a * (unit -> 'a page)
 
-effect Request : int -> (string list) res
-effect ContinueFrom : int * int -> (string list) res
+effect Request : int -> (string list) page
+effect ContinueFrom : int * int -> (string list) page
 
 let get n = perform (Request n)
 let get_from n = perform (ContinueFrom (n, n))
@@ -16,17 +16,17 @@ let database_server client =
   | () -> ()
   | effect (Request n) k ->
     let results = sublist 0 n database in
-    continue k (Res (results, fun () -> get_from n))
+    continue k (Page (results, fun () -> get_from n))
   | effect (ContinueFrom (start, size)) k ->
     let results = sublist start (start + size) database in
-    continue k (Res (results, fun () -> perform (ContinueFrom (start + size, size))))
+    continue k (Page (results, fun () -> perform (ContinueFrom (start + size, size))))
 
 let client () =
   print_endline "First page of results:";
-  let Res (results, next) = get 10 in
+  let Page (results, next) = get 10 in
   List.iter print_endline results;
   print_endline "\nGetting next page...";
-  let Res (results, next) = next () in
+  let Page (results, next) = next () in
   List.iter print_endline results
 
 let () =
