0a483b3a5602b93d514fe5c2796353e30321ef4f
songyahui <songyahuiapp@hotmail.com>
2022-01-14 10:37:13 +0800

    m

--- src/programs.t/files_paper.ml
+++ src/programs.t/files_paper.ml
@@ -5,3 +5,23 @@ let open_file n
 (*@ requires _^* @*)
 (*@ ensures Open(n) @*)
 = perform (Open n)
+
+let close_file n
+(*@ requires (_^* ).Open(n).(~Close(n))^* @*)
+(*@ ensures Close(n) @*)
+= perform (Close n)
+
+let file_9 () 
+(*@ requires emp @*)
+(*@ ensures Open(9).Close(9) @*)
+= open_file 9;
+  close_file 9
+
+let main 
+(*@ requires emp @*)
+(*@ ensures Open(9).Close(9) @*)
+= 
+  match file_9 () with 
+  | _ -> ()
+  | effect (Open n) k -> continue k ()
+  | effect (Close n) k -> continue k ()
