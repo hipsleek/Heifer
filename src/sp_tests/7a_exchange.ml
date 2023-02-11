a2e28a5b0175328865a47cc23fefa4e025cd33ea
songyahui <songyahuiapp@hotmail.com>
2023-02-11 13:18:33 +0800

    m

--- src/sp_tests/7a_exchange.ml
+++ src/sp_tests/7a_exchange.ml
@@ -1,33 +1,33 @@
-effect Exchange: int -> int
-
-let i = Sys.opaque_identity (ref 11) 
-
-let test () 
-(*@ req true
-  ensure Exchange(5) (\ x ->
-                requires emp
-                ensures  Exchange(10) \ y ->
-				       req emp
-					   ensures Norm /\ res=x+y) @*)
-= let res = perform (Exchange 5) in 
-  let res1= perform (Exchange 10) in
-  res+res1
+let test ()
+  (*@  req emp   @*)
+  (*@  ens Exchange(5) * Cont = (\ x ->
+           req emp
+           ens Exchange(8) * Cont = (\ x2 ->
+             req emp
+             ens res=x+x2 & Norm(res)
+         )
+      )
+   @*)
+  = let res = perform (Exchange 5) in
+    let res2 = perform (Exchange 8) i
+    res+res2
 
 let main
-(*@ req i->v
- ens i->5 /\ res=v+1@*)
-= 
-  match test () with 
-  | v -> v print_string ("Normal Rtn:" ^ string_of_int v ^ "\n")
-   (*Norm -> \ r ->
-            req ?
-			ens post
-      *)
-  | effect (Exchange x) k -> 
-    let oldv = !i in 
-    i := x; 
-    continue k oldv
-	(* Exchange(v) k ->
-      req i-> n
-			ens 
-      i-> v ; k n *)
+  req i-> init
+  ens i-> 8 /\ Norm(res) /\ res=init+5
+            /\ Cont = \ _ -> req false ens true // default
+  = match test () with
+    | v -> v print_string ("Normal Rtn:" ^ string_of_int v ^ "\n")
+          v+1
+    (*@ Norm(v) =
+       req emp
+       ens Norm(res) /\ res=v+1
+    @*)
+    | effect (Exchange x) k ->
+      let oldv = !i in
+      i := x;
+      continue k oldv
+    (*@ Exchange(x) k =
+       req i->oldv
+       ens i->x /\ Norm(res) /\ res=k oldv
+    @*)
