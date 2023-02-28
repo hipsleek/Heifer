f909f64f5f860520c7ac4cb5eaefb5c7ae3eb38e
Darius Foo <darius.foo.tw@gmail.com>
2023-02-28 14:49:46 +0800

    Elaborate

--- src/sp_tests/clone_is_tricky.ml
+++ src/sp_tests/clone_is_tricky.ml
@@ -1,19 +1,22 @@
 
 (* Example taken from https://github.com/ocaml-multicore/effects-examples/blob/master/multishot/clone_is_tricky.ml *)
 
-effect A : unit
-effect B : unit
+effect One : unit
+effect Multi : unit
 
 let run () =
-  match perform A with
-  | effect A k ->
-    perform B;
+  match perform One with
+  | effect One k ->
+    perform Multi;
     continue k ()
+    (* we get a crash here because One's continuation is resumed
+       more than once. When captured by continuations, linear
+       resumptions should be treated like resources + assertions. *)
   | _ -> ()
 
 let () =
   match run () with
-  | effect B k ->
+  | effect Multi k ->
     continue (Obj.clone_continuation k) ();
     continue k ()
   | _ -> ()
