aae34d07935c2887fb2e0dcf96818ec4dbd8f50a
Darius Foo <darius.foo.tw@gmail.com>
2023-03-20 18:03:38 +0800

    Some simple entailments

--- parsing/entail.ml
+++ parsing/entail.ml
@@ -120,27 +120,66 @@ S ::= req H | H & Norm | H & Eff | local v*
 N ::= \/ {S;..;S}
 *)
 
+let current_state : spec -> kappa =
+  fun sp ->
+    let rec loop current sp =
+      match sp with
+      | [] -> current
+      | HigherOrder _ :: s -> loop EmptyHeap s
+      | Ensures h :: s -> loop (SepConj (current, h)) s
+      | Exists _ :: s -> loop current s
+      | Require _ :: s ->
+        (* TODO look at this for pure constraints *)
+        loop current s
+      | NormalReturn _ :: _ -> failwith "unimplemented"
+      | RaisingEff _ :: _ -> failwith "unimplemented"
+    in loop EmptyHeap sp
+
 let check_heap_entail : kappa -> kappa -> kappa option =
-  fun n1 _n2 ->
-    Some n1
+  fun n1 n2 ->
+    match n1, n2 with
+    | EmptyHeap, EmptyHeap -> Some EmptyHeap
+    | PointsTo (s1, v1), PointsTo (s2, v2)
+      when String.equal s1 s2 && v1 = v2 ->
+        Some EmptyHeap
+    | _, _ -> None
+
+let rec check_staged_entail : spec -> spec -> spec option =
+  fun n1 n2 ->
+    (* check_heap_entail (current_state n1) (current_state n2) *)
+    Some (n1 @ n2)
+
+and check_staged_subsumption : spec -> spec -> bool =
+  fun n1 n2 ->
+    match n1, n2 with
+    | [], [] -> true
+    | d1 :: n3, d2 :: n4 ->
+      begin match d1, d2 with
+      | Require h1, Require h2 ->
+        (* contravariance *)
+        begin match check_heap_entail h2 h1 with
+        | Some _ -> true
+        | None -> false
+        end
+      | Ensures h1, Ensures h2 ->
+        (* covariance *)
+        begin match check_heap_entail h1 h2 with
+        | Some _ -> true
+        | None -> false
+        end
+      | _ -> failwith "unimplemented"
+      end && check_staged_subsumption n3 n4
+    | _ -> false
 
 let%expect_test "heap_entail" =
   let test l r = Format.printf "%s |- %s ==> %s@." (string_of_kappa l) (string_of_kappa r) (check_heap_entail l r |> string_of_option string_of_kappa) in
   test (PointsTo ("x", Num 1)) (PointsTo ("x", Num 1));
-  [%expect {| x->1 |- x->1 ==> Some x->1 |}]
-
-let check_staged_entail : spec -> spec -> spec option =
-  fun n1 n2 ->
-    Some (n1 @ n2)
+  [%expect {| x->1 |- x->1 ==> Some emp |}]
 
 let%expect_test "staged_entail" =
   let test l r = Format.printf "%s |= %s ==> %s@." (string_of_spec l) (string_of_spec r) (check_staged_entail l r |> string_of_option string_of_spec) in
   test [Ensures (PointsTo ("x", Num 1))] [Ensures (PointsTo ("x", Num 1))];
   [%expect {| x->1 |= x->1 ==> Some x->1; x->1 |}]
-
-let check_staged_subsumption : spec -> spec -> bool =
-  fun _n1 _n2 ->
-    true
 
 let%expect_test "staged_subsumption" =
   let test l r = Format.printf "%s %s %s@." (string_of_spec l) (if check_staged_subsumption l r then "|--" else "|-/-") (string_of_spec r) in
