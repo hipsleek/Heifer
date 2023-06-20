4792d4578bc8b079b34173cc4434dd8a1aff7d6d
songyahui <songyahuiapp@hotmail.com>
2023-06-20 12:47:58 +0800

    map with closure

--- src/examples/lists.ml
+++ src/examples/lists.ml
@@ -11,3 +11,15 @@ let id y = y
 let map_id ys
 (*@ Norm(emp, ys) @*)
 = map id ys
+
+let x = ref 0 ;; 
+
+let cl n = x := x + 1 ; n 
+
+(* Norm (x -> a /\ l = length(li) /\ ret=list' /\ list'=li, li)  *)
+let map_cl cl_in li = 
+  let l = List.length li in 
+  let list' = List.map (fun a -> cl_in a) li in 
+  assert (!x = l); 
+  list'
+
