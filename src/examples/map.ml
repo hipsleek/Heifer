badf8ec9a7440ef127e451a2120bb270c531102d
Darius Foo <darius.foo.tw@gmail.com>
2023-06-21 15:43:43 +0800

    More map tests

--- src/examples/map.ml
+++ src/examples/map.ml
@@ -4,13 +4,28 @@ let rec map f xs =
   | [] -> []
   | x :: xs1 -> f x :: map f xs1
 
-let incr x = x + 1
-
 let id y = y
 
 let map_id ys
 (*@ Norm(emp, ys) @*)
 = map id ys
+
+let succ x = x + 1
+
+let map_not_id_false ys
+(*@ Norm(emp, ys) @*)
+= map succ ys
+
+(* ghost function that specifies what mapping succ should return *)
+let rec succ_list xs =
+  match xs with
+  | [] -> []
+  | x :: xs1 -> succ x :: succ_list xs1
+
+(* we use succ_list in the statement of this lemma *)
+let map_succ ys
+(*@ ex r; succ_list(ys, r); Norm(emp, r) @*)
+= map succ ys
 
 (*
 let x = ref 0 ;; 
