f40ae1918bc14769ac6045c14d62324dcffc4e8c
Darius Foo <darius.foo.tw@gmail.com>
2023-06-22 11:54:04 +0800

    take and skip don't work for now

--- src/examples/take_skip.ml
+++ src/examples/take_skip.ml
@@ -1,8 +1,7 @@
 
 (* let (mutlist:(((int ref) list) ref)) = ref [ref 1; ref 2; ref 3; ref 4] *)
 
-
-let rec take n li =
+(* let rec take n li =
   (* (li:(int ref) list) : (int ref) list  =  *)
   if n = 0 then []
   else 
@@ -18,15 +17,33 @@ let rec skip n li =
     (match li with 
     | [] -> []
     | _ :: xs -> 
-      skip (n-1) xs)
+      skip (n-1) xs) *)
 
       (* ;; *)
-
-let take_skip n xs
-(*@ Norm(emp, []) @*)
-= skip n (take n xs)
 
 (* mutlist := take_n  3 (!mutlist);;
 mutlist := skip_n  2 (!mutlist);;
 
 assert (!mutlist = [ref 3]);; *)
+  (* (n:nat) (xs:list nat) := *)
+  (* (n:nat) (xs:list nat) := *)
+
+let rec take n xs =
+  if n = 0 then []
+  else
+    (match xs with
+    | [] -> []
+    | x :: xs1 -> x :: take (n-1) xs1)
+
+let rec skip n xs =
+  if n = 0 then xs
+  else
+    (match xs with
+    | [] -> []
+    | x :: xs1 -> skip (n-1) xs1)
+
+(* the induction hypothesis should be: forall xs, list nat, skip n (take n xs) = [] *)
+
+let take_skip n xs
+(*@ Norm(emp, []) @*)
+= skip n (take n xs)
