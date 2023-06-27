f6ce27c4daea89c63ca1fcd845d7d92739174eea
Darius Foo <darius.foo.tw@gmail.com>
2023-06-27 15:43:19 +0800

    Fix first/second call examples

--- src/examples/1_second_call_valid.ml
+++ src/examples/1_second_call_valid.ml
@@ -5,6 +5,21 @@ let call_ret f =
   let x = roll_dice () in 
   f x
 
+let main_false ()
+(*@ Norm(emp, 2) @*)
+= let x = ref 0 in
+  let cl i = 
+    let r = !x in 
+    x := i; 
+    r
+  in
+  (* L1 *)
+  (* cl 2; *)
+
+  assert (!x = 2);
+  (* L2: the call here is only valid after L1*)
+  call_ret cl
+
 let main ()
 (*@ Norm(emp, 2) @*)
 = let x = ref 0 in
--- src/examples/2_first_call_valid.ml
+++ src/examples/2_first_call_valid.ml
@@ -4,19 +4,18 @@ let call_ret f =
   let x = roll_dice () in 
   f x
 
-let main ()
-(*@ Norm(emp, 102) @*)
+let main_false ()
+(*@ Norm(emp, 1) @*)
 = let x = ref 2 in
 
   let cl i =
     let r = !x in
-    x := r + i;
+    x := r - i;
     r
   in
 
-  assert (!x = 2);
+  assert (!x=2);
   call_ret cl;
-
-  assert (!x = 102);
+  assert (!x=3);
   (* the function call is only valid at the first time *)
-  call_ret cl
+  1
