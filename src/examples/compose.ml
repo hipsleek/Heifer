7c3e8ea176ddf20cca256d04bb868c1d71ae0a47
Darius Foo <darius.foo.tw@gmail.com>
2023-06-28 21:03:45 +0800

    Infer more in compose

--- src/examples/compose.ml
+++ src/examples/compose.ml
@@ -1,24 +1,9 @@
 
-let compose f g x
-(*@
-  ex r_g; g(x, r_g);
-  ex r_f; f(r_g, r_f);
-  Norm (emp, r_f)
-@*)
-= f (g x)
+let compose f g x = f (g x)
 
-let f x 
-(*@ ex z;
-    req x->z;
-    Norm(x->z+1, x) @*)
-= x := !x + 1; x
+let f x = x := !x + 1; x
 
-let g x 
-(*@ ex u;
-    req x->u;
-    ex ret;
-    Norm(ret=u+u/\x->u+u, x) @*)
-= x := !x + !x; x
+let g x = x := !x + !x; x
 
 let caller1 () 
 (*@ ex w; Norm(w->3, 3) @*)
@@ -26,8 +11,8 @@ let caller1 ()
   let y1 = compose f g x in
   !y1
 
-(* let caller2 ()
+let caller2 ()
 (*@ ex w; Norm(w->4, 4) @*)
 = let y = ref 1 in 
   let y1 = compose g f y in
-  !y1 *)
+  !y1
