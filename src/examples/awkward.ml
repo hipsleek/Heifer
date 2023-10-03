7debca122d7062b1ad1e9404c174ee0036734897
Darius Foo <darius.foo.tw@gmail.com>
2023-10-03 13:03:36 +0800

    Fix awkward

--- src/examples/awkward.ml
+++ src/examples/awkward.ml
@@ -1,7 +1,14 @@
 
-let awkward =
-  let r = ref 0 in
-  fun f -> assert (!r mod 2 = 0); incr r; f(); incr r
+let incr x = x := !x + 1
 
-let () =
+let ignore _ = ()
+
+let main h
+= let r = ref 0 in
+  let awkward = fun f (*@ ex r r1; ens r->0; incr(r, r1); ex r2; h((), r2); ex r3; incr(r, r3) @*) ->
+    assert (!r = 0);
+    incr r;
+    f();
+    incr r
+  in
   ignore (awkward (fun () -> ignore (awkward (fun () -> ()))))
