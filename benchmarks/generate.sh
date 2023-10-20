c4ce545f787c6b672047acfd719a285a43604e3a
Darius Foo <darius.foo.tw@gmail.com>
2023-10-20 23:57:20 +0800

    Demote calls

--- benchmarks/generate.sh
+++ benchmarks/generate.sh
@@ -2,7 +2,7 @@
 
 # Someday https://github.com/realworldocaml/mdx/issues/245
 
-for f in src/examples/{calls.ml,iter.ml,closure.ml,map.ml,fold.ml,compose.ml,applyN.ml,all.ml,exception.ml}; do
+for f in src/examples/{iter.ml,closure.ml,map.ml,fold.ml,compose.ml,applyN.ml,all.ml,exception.ml}; do
   echo "
 \`\`\`sh
 \$ DEBUG=0 hyperfine --warmup 2 'dune exec parsing/hip.exe $f'
