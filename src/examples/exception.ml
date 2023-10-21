f00186b460a6df8882d5d02a0fce2f2dcbee69c2
Darius Foo <darius.foo.tw@gmail.com>
2023-10-21 10:42:19 +0800

    Fix mistake in exception example

--- src/examples/exception.ml
+++ src/examples/exception.ml
@@ -1,11 +1,11 @@
 
 let failing x
-(*@ ex i; req x->i; ex r; E(x->i+1 /\ x=3, r)
+(*@ ex i; req x->i; ex r; E(x->i+1 /\ i+1=3, r)
   \/
-   ex i; req x->i; ens x->i+1 /\ res=i+1 /\ ~x=3 @*)
+   ex i; req x->i; ens x->i+1 /\ res=i+1 /\ ~i+1=3 @*)
 =
   x := !x + 1;
-  if x = 3 then perform E else !x
+  if !x = 3 then perform E else !x
 
 (* TODO there is a bug in handler reasoning, result should be 1 *)
 
