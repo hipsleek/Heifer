86d374bb5341341f39cd1484b80b112be389e938
songyahui <songyahuiapp@hotmail.com>
2023-11-02 15:02:06 +0800

    tested ABEFG example! All good

--- benchmarks/effects/ocaml412/E_state_monad.ml
+++ benchmarks/effects/ocaml412/E_state_monad.ml
@@ -1,29 +1,35 @@
-open Printf
-
 effect Put : int -> unit
 effect Get : int
 
-let put v = perform (Put v)
-let get () = perform Get
+let put v 
+(*@ ex r; Put(emp, v, r); Norm(res=r) @*)
+= perform (Put v)
 
-let rec counter (c : int) =
-  let i = get ()
-  in if (i==0)
-      then c
-      else (put (i - 1); counter (c + 1))
 
-let _test () = let i = counter 0 in printf "%d\n" i
+let get () 
+(*@ ex r; Get(emp, r); Norm(res=r) @*)
+= perform Get
+
+let rec counter (c : int) 
+(*@  ex i; Get(emp, i); Norm(i=0, c)
+  \/ ex i; Get(emp, i); Norm(~i=0, i);  ex r; Put(emp, i-1, r); counter(c+1, res) @*)
+=
+  let i = get () in 
+  if (i==0) then c
+  else (put (i - 1); counter (c + 1))
 
 let read_n_write ()
+(*@  ex x; Get(emp, x);  ex r; Put(emp, x+1, r); ex i; Get(emp, i); Norm(emp, i) @*)
 = let x = get() in
   put(x+1);
   let i = get() in
-  printf "%d\n" i
+  i 
 
-let _ =
-  let s = ref 1 in
+let _test () 
+(*@  ex s;  Norm(s->2, 2) @*)
+= let s = ref 1 in
   match read_n_write () with (* n *)
   | x -> x
   | exception e -> raise e
-  | effect Get k -> let i : int = !s in continue k i
+  | effect Get k -> let i = !s in continue k i
   | effect (Put x) k -> s := x; continue k ()
--- benchmarks/effects/ocaml412/F_Exchange_State.ml
+++ benchmarks/effects/ocaml412/F_Exchange_State.ml
@@ -1,12 +1,12 @@
-open Printf
 
 effect Exchange : int -> int
-let l = ref 5
 
 let exchange (m:int)
+(*@ ex r; Exchange(emp, m, r); Norm(res=r) @*)
 = perform (Exchange m)
 
 let exc_handler (l:int ref) (new_v:int)
+(*@ ex z; req l->z; Norm(l->new_v, z) @*)
 =  match exchange new_v with
   | x -> x
   | exception e -> raise e
@@ -15,8 +15,8 @@ let exc_handler (l:int ref) (new_v:int)
      l := n;
      continue k old_v
 
-let _ =
+let test () 
+(*@ ex l;  Norm(l->10, 5) @*)
+= let l = ref 5 in
   let i = exc_handler l 10 in
-  printf "%d\n" i ;
-  let i = exc_handler l 10 in
-  printf "%d\n" i
+  i
