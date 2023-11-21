af88f34416a59da5bff5d9b3551127e7dad8616c
Darius Foo <darius.foo.tw@gmail.com>
2023-11-21 16:25:42 +0800

    Exception bug fixed itself

--- src/examples/exception.ml
+++ src/examples/exception.ml
@@ -6,10 +6,8 @@ let failing x
 = x := !x + 1;
   if !x = 3 then perform E else !x
 
-(* TODO there is a bug in handler reasoning, result should be 1 *)
-
 let main ()
-(*@ ex y; ens y->3 /\ res=3 @*)
+(*@ ex y; ens y->3 /\ res=1 @*)
 = let y = ref 2 in
   match failing y with
   | effect E k -> 1
