7963d2f57782a2a24f5de14111316f70c562ffeb
Darius Foo <darius.foo.tw@gmail.com>
2023-12-05 17:53:21 +0800

    Test why3 back end

--- provers/why3/provers.ml
+++ provers/why3/provers.ml
@@ -5,13 +5,6 @@ let handle f = f ()
 
 open Why3
 
-(* let fmla1 : Term.term = Term.t_or Term.t_true Term.t_false
-let prop_var_A : Term.lsymbol = Term.create_psymbol (Ident.id_fresh "A") []
-let prop_var_B : Term.lsymbol = Term.create_psymbol (Ident.id_fresh "B") []
-let atom_A : Term.term = Term.ps_app prop_var_A []
-let atom_B : Term.term = Term.ps_app prop_var_B []
-let fmla2 : Term.term = Term.t_implies (Term.t_and atom_A atom_B) atom_A *)
-
 type env = {
   mutable int_theory : Theory.theory option;
   env : Env.env;
@@ -52,25 +45,45 @@ let rec term_to_why3 env (t : term) =
   match t with
   | UNIT -> failwith "UNIT nyi"
   | Num i -> Term.t_nat_const i (* Term.t_int_const (BigInt.of_int i) *)
-  | Var _ -> failwith "Var nyi"
+  | Var v ->
+    let s : Term.lsymbol = Term.create_psymbol (Ident.id_fresh v) [] in
+    Term.ps_app s []
   | Plus (a, b) ->
-    let plus_symbol : Term.lsymbol =
-      Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix +"]
-    in
-    Term.t_app_infer plus_symbol [term_to_why3 env a; term_to_why3 env b]
-  | Minus (_, _) -> failwith "Minus nyi"
-  | Rel (_, _, _) -> failwith "Rel nyi"
-  | TTrue -> failwith "TTrue nyi"
-  | TFalse -> failwith "TFalse nyi"
-  | TAnd (_, _) -> failwith "TAnd nyi"
-  | TPower (_, _) -> failwith "TPower nyi"
-  | TTimes (_, _) -> failwith "TTimes nyi"
-  | TDiv (_, _) -> failwith "TDiv nyi"
-  | TOr (_, _) -> failwith "TOr nyi"
-  | TNot _ -> failwith "TNot nyi"
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix +"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Minus (a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix -"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Rel (EQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
+  | Rel (GT, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix >"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Rel (LT, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix <"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Rel (GTEQ, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix >="])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Rel (LTEQ, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix <="])
+      [term_to_why3 env a; term_to_why3 env b]
+  | TTrue -> Term.t_true
+  | TFalse -> Term.t_false
+  | TAnd (a, b) -> Term.t_and (term_to_why3 env a) (term_to_why3 env b)
+  | TOr (a, b) -> Term.t_and (term_to_why3 env a) (term_to_why3 env b)
+  | TNot a -> Term.t_not (term_to_why3 env a)
   | TApp (_, _) -> failwith "TApp nyi"
   | Nil -> failwith "Nil nyi"
   | TLambda (_, _, _) -> failwith "TLambda nyi"
+  | TPower (_, _) -> failwith "TPower nyi"
+  | TTimes (_, _) -> failwith "TTimes nyi"
+  | TDiv (_, _) -> failwith "TDiv nyi"
   | TList _ -> failwith "TList nyi"
   | TTupple _ -> failwith "TTupple nyi"
 
@@ -79,17 +92,29 @@ let rec pi_to_why3 env (pi : pi) =
   | True -> Term.t_true
   | False -> Term.t_false
   | Atomic (EQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
-  | Atomic (GT, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
-  | Atomic (LT, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
-  | Atomic (GTEQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
-  | Atomic (LTEQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
+  | Atomic (GT, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix >"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Atomic (LT, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix <"])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Atomic (GTEQ, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix >="])
+      [term_to_why3 env a; term_to_why3 env b]
+  | Atomic (LTEQ, a, b) ->
+    Term.t_app_infer
+      (Theory.ns_find_ls (use_int_theory env).Theory.th_export ["infix <="])
+      [term_to_why3 env a; term_to_why3 env b]
   | And (a, b) -> Term.t_and (pi_to_why3 env a) (pi_to_why3 env b)
-  | Or (_, _) -> failwith "nyi Or"
-  | Imply (_, _) -> failwith "nyi Imply"
-  | Not _ -> failwith "nyi Not"
+  | Or (a, b) -> Term.t_or (pi_to_why3 env a) (pi_to_why3 env b)
+  | Imply (a, b) -> Term.t_implies (pi_to_why3 env a) (pi_to_why3 env b)
+  | Not a -> Term.t_not (pi_to_why3 env a)
   | Predicate (_, _) -> failwith "nyi Predicate"
 
-let prove f goal =
+let prove f =
   (* reads the default config file *)
   let config : Whyconf.config = Whyconf.init_config None in
   (* the [main] section of the config file *)
@@ -100,16 +125,22 @@ let prove f goal =
   (* Format.printf "proving@."; *)
   (* Format.printf "load path: %s@." (string_of_list Fun.id (Whyconf.loadpath main)); *)
   let env = create_env env in
-  let ggoal = f env goal in
+  let ass, goal = f env in
 
   (* set up task *)
   let task1 : Task.task = None in
   (* let task1 = Task.use_export task1 env.int_theory in *)
   let task1 = apply_exports env task1 in
-  let goal_id1 : Decl.prsymbol =
-    Decl.create_prsymbol (Ident.id_fresh "goal1")
+  let task1 : Task.task =
+    Task.add_prop_decl task1 Decl.Paxiom
+      (Decl.create_prsymbol (Ident.id_fresh "ass1"))
+      ass
   in
-  let task1 : Task.task = Task.add_prop_decl task1 Decl.Pgoal goal_id1 ggoal in
+  let task1 : Task.task =
+    Task.add_prop_decl task1 Decl.Pgoal
+      (Decl.create_prsymbol (Ident.id_fresh "goal1"))
+      goal
+  in
   (* Format.printf "@[task 1 is:@\n%a@]@." Pretty.print_task task1; *)
   (* all the provers detected, from the config file *)
   (* let provers : Whyconf.config_prover Whyconf.Mprover.t = *)
@@ -131,13 +162,28 @@ let prove f goal =
   Format.printf "%s: %a@." prover_name
     (Call_provers.print_prover_result ?json:None)
     result1;
-  match result1.pr_answer with
-  | Valid -> true
-  | _ -> false
+  match result1.pr_answer with Valid -> true | _ -> false
 
-let entails_exists _env _left _ex _right =
+let type_to_why3 (t : typ) =
+  match t with
+  | Unit -> failwith "nyi Unit"
+  | List_int -> failwith "nyi List_int"
+  | Int -> Ty.ty_int
+  | Bool -> Ty.ty_bool
+  | Lamb -> failwith "nyi Lamb"
+  | TVar _ -> failwith "nyi TVar"
+
+let entails_exists tenv left ex right =
   (* Format.printf "@[formula 2 is:@ %a@]@." Pretty.print_term fmla2; *)
   (* Format.printf "@[formula 1 is:@ %a@]@." Pretty.print_term fmla1; *)
   (* prove (fun _ f -> f) fmla1; *)
-  let right = Atomic (EQ, Plus (Num 1, Num 1), Num 2) in
-  prove pi_to_why3 right
+  (* let right = Atomic (EQ, Plus (Num 1, Num 1), Num 2) in *)
+  let vars =
+    List.map
+      (fun v ->
+        let ty = SMap.find_opt v tenv |> Option.value ~default:Int in
+        Term.create_vsymbol (Ident.id_fresh "x") (type_to_why3 ty))
+      ex
+  in
+  prove (fun env ->
+      (pi_to_why3 env left, Term.t_exists_close vars [] (pi_to_why3 env right)))
