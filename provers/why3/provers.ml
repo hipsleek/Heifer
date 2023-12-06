9c5e60d7061c95e053fa0bc40a84a9ba826df1a0
Darius Foo <darius.foo.tw@gmail.com>
2023-12-06 16:33:51 +0800

    Include theories only as needed

--- provers/why3/provers.ml
+++ provers/why3/provers.ml
@@ -1,15 +1,17 @@
 open Hipcore
 include Hiptypes
 
+(* let debug = true *)
+let debug = false
 let handle f = f ()
 
 open Why3
 
 let prover_configs : Whyconf.config_prover SMap.t ref = ref SMap.empty
-let prover_z3 = "Z3" 
+let prover_z3 = "Z3"
 
 (* top-level side effects *)
-let why3_config = Whyconf.init_config None 
+let why3_config = Whyconf.init_config None
 let why3_config_main = Whyconf.get_main why3_config
 let why3_env : Env.env = Env.create_env (Whyconf.loadpath why3_config_main)
 
@@ -17,23 +19,24 @@ let load_prover_config name : Whyconf.config_prover =
   let fp = Whyconf.parse_filter_prover name in
   let provers = Whyconf.filter_provers why3_config fp in
   if Whyconf.Mprover.is_empty provers then begin
-    Format.eprintf "Prover %s not installed or not configured@." name;
+    Format.printf "Prover %s not installed or not configured@." name;
     exit 1
   end
   else begin
     (* Format.printf "Versions of %s found:" name;
-    Whyconf.(
-      Mprover.iter
-        (fun k _ -> Format.printf " %s/%s" k.prover_version k.prover_altern)
-        provers);
-    Format.printf "@."; *)
+       Whyconf.(
+         Mprover.iter
+           (fun k _ -> Format.printf " %s/%s" k.prover_version k.prover_altern)
+           provers);
+       Format.printf "@."; *)
     (* returning an arbitrary one *)
     snd (Whyconf.Mprover.min_binding provers)
   end
 
 let ensure_prover_loaded name =
   match SMap.find_opt name !prover_configs with
-  | None -> prover_configs := SMap.add name (load_prover_config name) !prover_configs
+  | None ->
+    prover_configs := SMap.add name (load_prover_config name) !prover_configs
   | Some _ -> ()
 
 let get_prover_config name = SMap.find name !prover_configs
@@ -41,81 +44,147 @@ let get_prover_config name = SMap.find name !prover_configs
 let load_prover_driver pc name =
   try Driver.load_driver_for_prover why3_config_main why3_env pc
   with e ->
-    Format.eprintf "Failed to load driver for %s: %a@." name
+    Format.printf "Failed to load driver for %s: %a@." name
       Exn_printer.exn_printer e;
-      raise e
-
-
+    raise e
 
 type env = {
   mutable names : Term.lsymbol SMap.t;
-  int_theory : Theory.theory;
+  mutable int_theory : Theory.theory option;
+  mutable bool_theory : Theory.theory option;
+  mutable list_theory : Theory.theory option;
   tenv : typ SMap.t;
   quantified : Term.vsymbol SMap.t;
 }
 
+let int_theory_needed env =
+  match env.int_theory with
+  | None -> env.int_theory <- Some (Env.read_theory why3_env ["int"] "Int")
+  | Some _ -> ()
 
-let type_to_why3 (t : typ) =
+let bool_theory_needed env =
+  match env.bool_theory with
+  | None -> env.bool_theory <- Some (Env.read_theory why3_env ["bool"] "Bool")
+  | Some _ -> ()
+
+let list_theory_needed env =
+  match env.list_theory with
+  | None -> env.list_theory <- Some (Env.read_theory why3_env ["list"] "List")
+  | Some _ -> ()
+
+let get_theory_symbol th sym =
+  match th with
+  | None -> failwith (Format.asprintf "theory with symbol %s not loaded" sym)
+  | Some t -> Theory.ns_find_ls t.Theory.th_export [sym]
+
+let get_theory_type_symbol th sym =
+  match th with
+  | None -> failwith (Format.asprintf "theory with symbol %s not loaded" sym)
+  | Some t -> Theory.ns_find_ts t.Theory.th_export [sym]
+
+let type_to_why3 env (t : typ) =
   match t with
   | Unit -> Ty.ty_tuple []
-  | List_int -> failwith "nyi List_int"
-  | Int -> Ty.ty_int
-  | Bool -> Ty.ty_bool
+  | List_int ->
+    int_theory_needed env;
+    list_theory_needed env;
+    Ty.ty_app (get_theory_type_symbol env.list_theory "list") [Ty.ty_int]
+  | Int ->
+    int_theory_needed env;
+    Ty.ty_int
+  | Bool ->
+    bool_theory_needed env;
+    Ty.ty_bool
   | Lamb -> failwith "nyi Lamb"
-  | TVar _ -> Ty.ty_int (* default to int *)
-
+  | TVar _ ->
+    (* failwith "unknown type" *)
+    (* default to int *)
+    int_theory_needed env;
+    Ty.ty_int
 
 let rec term_to_why3 env (t : term) =
+  if debug then Format.printf "term %s@." (Hipcore.Pretty.string_of_term t);
   match t with
   | UNIT -> Term.t_tuple []
-  | Num i -> Term.t_nat_const i
+  | Num i ->
+    int_theory_needed env;
+    Term.t_nat_const i
   | Var v ->
     let ty =
-      SMap.find_opt v env.tenv |> Option.value ~default:Int |> type_to_why3
+      SMap.find_opt v env.tenv |> Option.value ~default:Int |> type_to_why3 env
     in
     let name =
       match SMap.find_opt v env.quantified with
       | Some v -> Term.t_var v
       | None ->
-        match SMap.find_opt v env.names with
+        (match SMap.find_opt v env.names with
         | None ->
           let name = Ident.id_fresh v in
           let sym = Term.create_lsymbol name [] (Some ty) in
           env.names <- SMap.add v sym env.names;
           Term.t_app sym [] (Some ty)
-        | Some vv -> Term.t_app vv [] (Some ty)
+        | Some vv -> Term.t_app vv [] (Some ty))
     in
     name
   | Plus (a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix +"])
+      (get_theory_symbol env.int_theory "infix +")
       [term_to_why3 env a; term_to_why3 env b]
   | Minus (a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix -"])
+      (get_theory_symbol env.int_theory "infix -")
       [term_to_why3 env a; term_to_why3 env b]
-  | Rel (EQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
+  | Rel (EQ, a, b) ->
+    let a1 = term_to_why3 env a in
+    let b1 = term_to_why3 env b in
+    Term.t_equ a1 b1
   | Rel (GT, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix >"])
+      (get_theory_symbol env.int_theory "infix >")
       [term_to_why3 env a; term_to_why3 env b]
   | Rel (LT, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix <"])
+      (get_theory_symbol env.int_theory "infix <")
       [term_to_why3 env a; term_to_why3 env b]
   | Rel (GTEQ, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix >="])
+      (get_theory_symbol env.int_theory "infix >=")
       [term_to_why3 env a; term_to_why3 env b]
   | Rel (LTEQ, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix <="])
+      (get_theory_symbol env.int_theory "infix <=")
       [term_to_why3 env a; term_to_why3 env b]
-  | TTrue -> Term.t_true
-  | TFalse -> Term.t_false
-  | TAnd (a, b) -> Term.t_and (term_to_why3 env a) (term_to_why3 env b)
-  | TOr (a, b) -> Term.t_and (term_to_why3 env a) (term_to_why3 env b)
-  | TNot a -> Term.t_not (term_to_why3 env a)
+  | TTrue ->
+    bool_theory_needed env;
+    Term.t_bool_true
+  | TFalse ->
+    bool_theory_needed env;
+    Term.t_bool_false
+  | TAnd (a, b) ->
+    bool_theory_needed env;
+    Term.t_app_infer
+      (get_theory_symbol env.bool_theory "andb")
+      [term_to_why3 env a; term_to_why3 env b]
+    (* Term.fs_app  [] Ty.ty_bool *)
+    (* Term.t_and (term_to_why3 env a) (term_to_why3 env b) *)
+  | TOr (a, b) ->
+    (* Term.t_and (term_to_why3 env a) (term_to_why3 env b) *)
+    bool_theory_needed env;
+    Term.t_app_infer
+      (get_theory_symbol env.bool_theory "orb")
+      [term_to_why3 env a; term_to_why3 env b]
+  | TNot a ->
+    (* Term.t_not (term_to_why3 env a) *)
+    bool_theory_needed env;
+    Term.t_app_infer
+      (get_theory_symbol env.bool_theory "notb")
+      [term_to_why3 env a]
   | TApp (_, _) -> failwith "TApp nyi"
   | Nil -> failwith "Nil nyi"
   | TLambda (_, _, _) -> failwith "TLambda nyi"
@@ -125,27 +194,34 @@ let rec term_to_why3 env (t : term) =
   | TList _ -> failwith "TList nyi"
   | TTupple _ -> failwith "TTupple nyi"
 
-
 let rec pi_to_why3 env (pi : pi) =
+  if debug then Format.printf "pi %s@." (Hipcore.Pretty.string_of_pi pi);
   match pi with
   | True -> Term.t_true
   | False -> Term.t_false
-  | Atomic (EQ, a, b) -> Term.t_equ (term_to_why3 env a) (term_to_why3 env b)
+  | Atomic (EQ, a, b) ->
+    let a1 = term_to_why3 env a in
+    let b1 = term_to_why3 env b in
+    Term.t_equ a1 b1
   | Atomic (GT, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix >"])
+      (get_theory_symbol env.int_theory "infix >")
       [term_to_why3 env a; term_to_why3 env b]
   | Atomic (LT, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix <"])
+      (get_theory_symbol env.int_theory "infix <")
       [term_to_why3 env a; term_to_why3 env b]
   | Atomic (GTEQ, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix >="])
+      (get_theory_symbol env.int_theory "infix >=")
       [term_to_why3 env a; term_to_why3 env b]
   | Atomic (LTEQ, a, b) ->
+    int_theory_needed env;
     Term.t_app_infer
-      (Theory.ns_find_ls env.int_theory.Theory.th_export ["infix <="])
+      (get_theory_symbol env.int_theory "infix <=")
       [term_to_why3 env a; term_to_why3 env b]
   | And (a, b) -> Term.t_and (pi_to_why3 env a) (pi_to_why3 env b)
   | Or (a, b) -> Term.t_or (pi_to_why3 env a) (pi_to_why3 env b)
@@ -154,17 +230,24 @@ let rec pi_to_why3 env (pi : pi) =
   | Predicate (_, _) -> failwith "nyi Predicate"
 
 let create_env tenv qtf =
-  let int_theory = Env.read_theory why3_env ["int"] "Int" in
+  let initial =
+    {
+      names = SMap.empty;
+      tenv;
+      quantified = SMap.empty;
+      int_theory = None;
+      bool_theory = None;
+      list_theory = None;
+    }
+  in
   let quantified =
     qtf
-      |> List.map (fun v ->
-        let ty = SMap.find_opt v tenv |> Option.value ~default:Int in
-        v, Term.create_vsymbol (Ident.id_fresh v) (type_to_why3 ty))
-      |> List.to_seq
-      |> SMap.of_seq
+    |> List.map (fun v ->
+           let ty = SMap.find_opt v tenv |> Option.value ~default:Int in
+           (v, Term.create_vsymbol (Ident.id_fresh v) (type_to_why3 initial ty)))
+    |> List.to_seq |> SMap.of_seq
   in
-  { int_theory; names = SMap.empty; tenv; quantified }
-
+  { initial with quantified }
 
 let prove tenv qtf f =
   let env = create_env tenv qtf in
@@ -172,9 +255,17 @@ let prove tenv qtf f =
 
   (* set up task *)
   let task1 : Task.task = None in
-  let task1 = Task.use_export task1 env.int_theory in
-  (* let debug = true in *)
-  let debug = false in
+  let task1 =
+    (* match env.int_theory with
+       | None -> task1
+       | Some t -> Task.use_export task1 t *)
+    Task.use_export task1 (Env.read_theory why3_env ["int"] "Int")
+  in
+  let task1 =
+    match env.bool_theory with
+    | None -> task1
+    | Some t -> Task.use_export task1 t
+  in
 
   let task1 =
     List.fold_right
@@ -182,10 +273,10 @@ let prove tenv qtf f =
       (SMap.bindings env.names) task1
   in
 
-  if debug then
-    Format.printf "assumptions: %a@." Pretty.print_term ass;
-  if debug then
-    Format.printf "goal: %a@." Pretty.print_term goal;
+  (* if debug then *)
+  (* Format.printf "tenv: %s@." (Hipcore.Pretty.string_of_typ_env tenv); *)
+  if debug then Format.printf "assumptions: %a@." Pretty.print_term ass;
+  if debug then Format.printf "goal: %a@." Pretty.print_term goal;
   let task1 : Task.task =
     Task.add_prop_decl task1 Decl.Paxiom
       (Decl.create_prsymbol (Ident.id_fresh "ass1"))
@@ -196,12 +287,16 @@ let prove tenv qtf f =
       (Decl.create_prsymbol (Ident.id_fresh "goal1"))
       goal
   in
+  (* if debug then
+     Format.printf "task: %a@." Pretty.print_task task1; *)
   ensure_prover_loaded prover_z3;
   let pc = get_prover_config prover_z3 in
   let result1 =
     Call_provers.wait_on_call
-      (Driver.prove_task ~limit:Call_provers.empty_limit ~config:why3_config_main
-         ~command:pc.Whyconf.command (load_prover_driver pc prover_z3) task1)
+      (Driver.prove_task ~limit:Call_provers.empty_limit
+         ~config:why3_config_main ~command:pc.Whyconf.command
+         (load_prover_driver pc prover_z3)
+         task1)
   in
   if debug then
     Format.printf "%s: %a@." "Z3"
@@ -210,6 +305,17 @@ let prove tenv qtf f =
   match result1.pr_answer with Valid -> true | _ -> false
 
 let entails_exists tenv left ex right =
-  prove tenv ex (fun env ->
-      (pi_to_why3 env left,
-        Term.t_exists_close (SMap.bindings env.quantified |> List.map snd) [] (pi_to_why3 env right)))
+  try
+    prove tenv ex (fun env ->
+        ( pi_to_why3 env left,
+          Term.t_exists_close
+            (SMap.bindings env.quantified |> List.map snd)
+            [] (pi_to_why3 env right) ))
+  with e ->
+    (* let s = Format.asprintf "%a" Exn_printer.exn_printer e in *)
+    (* let err = "Not a term" in *)
+    (* if String.sub s 0 (String.length err) = err then raise e; *)
+    Format.printf "an error occurred, assuming proof failed: %a@."
+      Exn_printer.exn_printer e;
+    (* Printexc.print_backtrace stdout; *)
+    false
