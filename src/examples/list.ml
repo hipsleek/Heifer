89314427e82cee31f93cb8c42b65abc49825ee3c
Emily Ong <emilyohq11@gmail.com>
2023-12-26 22:24:14 +0800

    Add list programs for generated specs

---
+++ src/examples/list.ml
@@ -0,0 +1,29 @@
+
+let rec snoc lst x =
+  match lst with
+  | [] -> [x]
+  | y :: ys -> y :: snoc ys x
+
+let rec reverse lst =
+  match lst with
+  | [] -> []
+  | x :: xs -> snoc (reverse xs) x 
+  
+let rec subsequence sub lst =
+  match lst with
+  | [] -> (match sub with
+    | [] -> true
+    | _ -> false)
+  | x :: xs -> (match sub with
+    | [] -> true
+    | y :: ys -> if x == y then subsequence ys xs else subsequence sub xs)
+
+let rec find lst y =
+  match lst with
+  | [] -> 0
+  | x :: xs -> if x = y then 0 else 1 + (find xs y)
+
+let rec replace lst x y =
+  match lst with
+  | [] -> []
+  | h :: tail -> if h = x then y :: replace tail x y else h :: replace tail x y
