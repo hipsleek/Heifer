066f807dbf7e8c1041ce01f2bc835c47342aba5a
Darius Foo <darius.foo.tw@gmail.com>
2024-01-05 08:49:11 +0800

    Change how prover is specified

--- provers/native/provers.ml
+++ provers/native/provers.ml
@@ -1,7 +1,7 @@
 
 let entails_exists env left ex right =
-  match Sys.getenv_opt "WHY3" with
-  | None -> Prover_z3.entails_exists env left ex right
-  | Some _ -> Prover_why3.entails_exists env left ex right
+  match Sys.getenv_opt "PROVER" with
+  | Some "WHY3" -> Prover_why3.entails_exists env left ex right
+  | Some "Z3" | Some _ | None -> Prover_z3.entails_exists env left ex right
 
 let handle f = f ()
