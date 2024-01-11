63be2d23a0258458b0347c018b134c84234a1e26
Emily Ong <emilyohq11@gmail.com>
2024-01-11 17:25:09 +0800

    Generalise repeat_with_n

--- src/examples/repeat_with_n.ml
+++ src/examples/repeat_with_n.ml
@@ -2,11 +2,14 @@
 (* Adapted from https://github.com/FabianWolff/closure-examples/blob/master/repeat_with_n.rs *)
 let rec repeat_with_n f n =
   if n = 0 then []
-  (* Currently, this is a cons instead of a snoc *)
   else f () :: repeat_with_n f (n - 1)
 
-let test1 () (* FIXME *)
-(*@ ens res=[18;16;14;12] @*)
-= let c = ref 5 in
-  let cl () = c := !c + 1; !c + !c in
-  repeat_with_n cl 4
+let rec integers n =
+  if n <= 0 then []
+  else n :: integers (n - 1)
+
+let test1 n (* FIXME *)
+(*@ ex r; integers(n, r); ens res=r @*)
+= let c = ref 0 in
+  let cl () = c := !c + 1; !c in
+  repeat_with_n cl n
