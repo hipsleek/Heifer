aa8c3e78357d770bffa95af2eceb530830160fd2
Emily Ong <emilyohq11@gmail.com>
2024-01-11 19:04:01 +0800

    Use function spec for call_ret

--- src/examples/closure.ml
+++ src/examples/closure.ml
@@ -58,21 +58,23 @@ let closure_with_history_invariant i j
   j := cl()
 
 (* Section 3.3 in Modular Specification and Verification of Closures in Rust *)
-(* Temporary workaround because function parameter preconditions are unsupported *)
-let call_ret x
-(*@ req x>0; ens res>0 @*)
-= x
+let roll_dice () (*@ ens res>0 @*) = 4 (* FIXME: Should be random *)
+let call_ret f
+(*@ req f <: (fun v r -> req v>0; ens r>0) @*)
+= let x = roll_dice () in f x
 
-let closure_with_hof_fail () (* Gives `req false` *)
+let closure_with_hof_false ()
+(*@ ens T @*)
 = let x = ref (-1) in
   let cl i = let r = !x in x := !x + i; r in
-  call_ret !x (* -1 *)
+  (* x is still -1 *)
+  call_ret cl
 
 let closure_with_hof_ok ()
 = let x = ref (-1) in
   let cl i = let r = !x in x := !x + i; r in
-  cl 2;
-  call_ret !x
+  cl 2; (* x is now 1 *)
+  call_ret cl
 
 (* https://ilyasergey.net/CS6217/_static/slides/04-FunLog.pdf *)
 let min_max_plus x y min max
