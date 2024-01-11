5294a345d313d60270e5482c1f57d4719cb3afa2
Emily Ong <emilyohq11@gmail.com>
2024-01-11 18:53:03 +0800

    Fix specification in counter and delegation

--- src/examples/counter.ml
+++ src/examples/counter.ml
@@ -2,22 +2,24 @@
 (* https://github.com/FabianWolff/closure-examples/blob/master/counter.rs *)
 
 let foo f
-(*@ req f <: (fun r -> ens r>=1) @*)
+(*@ req f <: (fun v r -> ens r>=2) @*)
 = ()
 
-let counter_ok ()
+let counter_ok () (* FIXME *)
+(*@ ens T @*)
 = let x = ref 0 in
   let inc ()
-  (*@ ex v; req x->v; ens x->v+1/\res=v @*)
+  (*@ ex v; req x->v/\v>=0; ens x->v+1/\res=v @*)
   = let r = !x in x := !x + 1; r in
   inc ();
   inc ();
-  foo inc
+  foo inc (* Succeeds because the result of the closure is 2 *)
 
-let counter_fail () (* FIXME *)
+let counter_false ()
+(*@ ens T @*)
 = let x = ref 0 in
   let inc ()
-  (*@ ex v; req x->v; ens x->v+1/\res=v @*)
+  (*@ ex v; req x->v/\v>=0; ens x->v+1/\res=v @*)
   = let r = !x in x := !x + 1; r in
   inc ();
-  foo inc
+  foo inc (* Fails because the result of the closure is 1 *)
--- src/examples/delegation.ml
+++ src/examples/delegation.ml
@@ -2,7 +2,23 @@
 (* https://github.com/FabianWolff/closure-examples/blob/master/delegation.rs *)
 
 let f y h g (* FIXME *)
-(*@ ex yv; req y->yv; ens y->4 @*)
+(*@
+  ex yv;
+  req y->yv
+    (* Ensures that h and g do not change anything on the heap *)
+    /\ h <: (fun v r -> req T; ens T/\res=r)
+    /\ g <: (fun v r -> req T; ens T/\res=r);
+  ens y->4
+@*)
+(**
+  The specification requires that:
+  - Preconditions of h and g initially hold
+  - Assignment to x does not interfere with the preconditions of h and g
+  - h and g does not capture y and the assignment to y is guaranteed to be
+    non-interfering
+  - Invoking h does not interfere with the precondition of g
+  (with reference from Example 10 from Specification and verification of closures)
+*)
 = let x = ref 3 in
   y := 4;
   (* Ensure that h and g are called successfully *)
