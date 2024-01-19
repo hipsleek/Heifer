917ca98519131a91a60a06401fb59a180015a821
Emily Ong <emilyohq11@gmail.com>
2024-01-19 13:53:01 +0800

    Generalise accumulator factory spec

--- src/examples/later/accumulator_factory.ml
+++ src/examples/later/accumulator_factory.ml
@@ -3,15 +3,26 @@
 
 let accumulator init =
   let total = ref init in
-  fun n ->
-    total := !total + n;
-    !total
+  let acc x
+  (*@ ex t; req total->t; ens total->t+x @*)
+  = total := !total + x; !total in
+  acc
 
-let test n
-(*@ ens res=n @*)
-= let acc = accumulator 0 in
+external sum : int -> int -> int -> int = "accumulator_factory.Extras.sum"
+
+let sum_aux x n init =
+  let total = ref init in
+  let rec aux x n =
+    if n = 0 then !total
+    else let _ = total := !total + x in aux x (n - 1)
+  in
+  aux n
+
+let test init x n
+(*@ ex r; sum_aux(x,n,init,r); ens res=r @*)
+= let acc = accumulator init in
   let rec repeat x n =
     if n = 0 then (acc 0)
-    else acc x; repeat x (n - 1)
+    else let r = acc x in repeat x (n - 1)
   in
-  repeat 1 n
+  repeat x n
