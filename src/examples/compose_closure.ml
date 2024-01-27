720cf29bf3396fe7075b80ae9a55389e37d79975
Darius Foo <darius.foo.tw@gmail.com>
2024-01-27 11:07:55 +0800

    Add compose with shared state

--- src/examples/compose_closure.ml
+++ src/examples/compose_closure.ml
@@ -13,6 +13,22 @@ let compose_state_1 ()
 
 let compose_state_2 ()
 (*@ ex w; ens w->4/\res=4 @*)
-= let y = ref 1 in 
+= let y = ref 1 in
   let y1 = compose g f y in
   !y1
+
+let compose_shared_1 ()
+(*@ ex w; ens w->4/\res=4 @*)
+= let y = ref 1 in
+  let f x = y := !y + 1; x in
+  let g x = y := !y + !y; x in
+  let _ = compose g f 1 in
+  !y
+
+let compose_shared_2 ()
+(*@ ex w; ens w->3/\res=3 @*)
+= let y = ref 1 in
+  let f x = y := !y + 1; x in
+  let g x = y := !y + !y; x in
+  let _ = compose f g 1 in
+  !y
