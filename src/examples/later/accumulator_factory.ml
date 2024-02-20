856a82ce2ae406451445a00e0bfef097745e39c7
Emily Ong <emilyohq11@gmail.com>
2024-02-20 22:23:12 +0800

    Fix nits

--- src/examples/later/accumulator_factory.ml
+++ src/examples/later/accumulator_factory.ml
@@ -10,15 +10,8 @@ let accumulator init =
 
 let test x n init (* FIXME *)
 (*@ req n>=0; ens res=init+(n*.x) @*)
-= assert (n >= 0);
-  let acc = accumulator init in
+= let acc = accumulator init in
   let rec repeat x k
-  (*@
-    req k=n; ex v; acc(0,v); req v=init+(n*.x); ens res=v
-    \/ req 0<=k/\k<n;
-       ex v; acc(x,v); req v=init+(k*.x+x);
-       ex r; repeat(x,k+1,r); req r=init+(n*.x); ens res=r
-  @*)
   = if k = n then acc 0
     else (acc x; repeat x (k + 1))
   in
