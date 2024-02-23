94365bd64656cc34b9771b1b306c5e544edcc054
Darius Foo <darius.foo.tw@gmail.com>
2024-02-23 08:48:57 +0800

    Working on acc factory

--- src/examples/later/accumulator_factory.ml
+++ src/examples/later/accumulator_factory.ml
@@ -1,4 +1,4 @@
-
+(* 
 (* https://rosettacode.org/wiki/Accumulator_factory *)
 
 let accumulator init =
@@ -25,4 +25,40 @@ let test init x n
     if n = 0 then (acc 0)
     else let r = acc x in repeat x (n - 1)
   in
+  repeat x n *)
+
+
+(* https://rosettacode.org/wiki/Accumulator_factory *)
+
+let accumulator init
+(*@ ex total; ens total->init/\res=(fun x r (*@ ex t; req total->t; ens total->t+x/\r=t+x @*)) @*)
+= let total = ref init in
+  let acc x
+  (*@ ex t; req total->t; ens total->t+x/\res=t+x @*)
+  = total := !total + x; !total
+  in
+  acc
+
+let test x n init (* FIXME *)
+(* req n>=0; ens res=init+(n*.x) @*)
+= let acc = accumulator init in
+  let rec repeat x k
+  (*@ ens k<=0; ex i; req total->i; ens total->i/\res=i
+    \/ ens k>0; ex i; req total->i; ens total->i+x; repeat(x, k-1, res)
+  @*)
+  = if k <= 0 then acc 0
+    else (acc x; repeat x (k - 1))
+  in
+  (* req n>=0; ens res=init+(n*.x) @*)
+  (* let intermediate x n
+    (* ens k<=0; ex total i; req total->i; ens total->i/\res=i
+    \/ ens k>0; ex total i; req total->i; ens total->i+x; repeat(x, k-1, res)
+    @*)
+    = repeat x n
+  in
+  intermediate x n *)
   repeat x n
+
+(* let use x n init
+(* req n>=0; ens res=init+(n*.x) @*)
+= test x n init *)
