971da7aac56fc8a56ee654f6a35b63e1b5a2d4f6
Darius Foo <darius.foo.tw@gmail.com>
2024-06-11 13:38:53 +0800

    Examples

--- src/reset_shift_progs/translate.ml
+++ src/reset_shift_progs/translate.ml
@@ -1,4 +1,8 @@
 
+let hello_lambda ()
+(*@ ens res=2 @*)
+= (fun x -> x) 2
+
 let undelimited () =
   1 + shift k 1
 
@@ -15,18 +19,48 @@ let hello3 ()
 = let f x = x + x in
   reset (1 + shift k (k (f 1)))
 
-let hello4 ()
+let hello41 ()
 (*@ ens res=4 @*)
 = let f x = x + x in
   (reset (shift k (fun x -> k (f x)))) 2
 
-(* let get_int () =
-  shift k (fun x -> k x)
-
-let main () =
-  (reset (get_int () ^ get_int ())) "a" "b" *)
-
-(* let hello_lambda ()
+let hello4 ()
 (*@ ens res=4 @*)
-= let f x = x + x in
-  (fun x -> x) 2 *)
+= (reset (shift k (fun x -> k x))) 4
+
+let hello5 ()
+(*@ ens res=5 @*)
+= (reset (shift k (fun x -> k x) + 2)) 3
+
+let hello6 ()
+(*@ ens res=6 @*)
+= ((reset (shift k (fun x -> k x) + shift k (fun x -> k x))) 2) 4
+
+let hello7 ()
+(*@ ens res=7 @*)
+= ((reset (shift k (fun x -> k x) + shift k (fun x -> k x))) 6) 1
+
+let hello_eta ()
+(*@ ens res=2 @*)
+= (reset (shift k k)) 2
+
+let hello8 ()
+(*@ ens res=8 @*)
+= ((reset (shift k k + shift k k)) 3) 5
+
+(* let main1 ()
+(*@ ens res=3 @*)
+= let get_int () = shift k (fun x -> k x) in
+  ((reset (get_int () + 1)) 2) *)
+
+  (* ((reset (get_int () + get_int ())) 1) 2 *)
+
+(* ((reset (get_int () ^ get_int ())) "a") "b" *)
+
+(* shift k k *)
+
+(* let get_int () = shift k (fun x -> k x)
+
+let main1 ()
+(*@ ens res=3 @*)
+= ((reset (get_int () + 1)) 2) *)
