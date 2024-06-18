097826ff484ce46a782b531ace392bff8eb500a8
Darius Foo <darius.foo.tw@gmail.com>
2024-06-18 22:00:52 +0800

    Simplify parser build, put libraries under heifer

---
+++ parsing/generate_parser.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cp $(opam var lib)/menhirLib/menhirLib.ml camlinternalMenhirLib.ml
+cp $(opam var lib)/menhirLib/menhirLib.mli camlinternalMenhirLib.mli
+
+#	 Derived from promote-menhir in https://github.com/ocaml/ocaml/blob/trunk/Makefile.menhir
+#	 Other useful options: --strict --explain --dump --log-grammar 1 --log-automaton 1 --require-aliases,
+#	 --infer (but then --ocamlc needs to be supplied with all the right includes)
+menhir --lalr --unused-token COMMENT --unused-token DOCSTRING --unused-token EOL --unused-token GREATERRBRACKET --fixed-exception --table --strategy simplified --base parser parser.mly
+
+cat parser.ml | sed 's/MenhirLib/CamlinternalMenhirLib/g' > parser1.ml
+mv parser1.ml parser.ml
+cat parser.mli | sed 's/MenhirLib/CamlinternalMenhirLib/g' > parser1.mli
+mv parser1.mli parser.mli
