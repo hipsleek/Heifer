89328e40d982640fe02c037e0297e13fa0f6bbd0
Dan Baterisna <alden.baterisna@yahoo.com>
2025-05-23 15:27:34 +0800

    new spec: include spec attribute in parsed core_lang

--- lib/heifer-parsing/annotation.ml
+++ lib/heifer-parsing/annotation.ml
@@ -1,2 +1,21 @@
 
-let extract_spec_attribute (_ : Parsetree.attribute list) : Hipcore.Hiptypes.staged_spec option = None
+open Parsing
+
+let parse_staged_spec spec = Parser.parse_staged_spec Lexer.token (Lexing.from_string spec)
+
+let parse_spec_attribute (attr : Parsetree.attribute) : Hipcore.Hiptypes.staged_spec option =
+  let spec_attribute_name = "spec" in
+  let open Ocaml_compiler.Ocaml_common.Parsetree in
+  (* TODO use ppxlib to do this matching instead; it would be a lot cleaner... *)
+  match attr with
+  | { attr_name = {txt = attr_name; _};
+      attr_payload = PStr [{pstr_desc = Pstr_eval ({pexp_desc = Pexp_constant {pconst_desc = Pconst_string (annotation, _, _); _}; _}, _); _}];
+      _} when String.equal attr_name spec_attribute_name -> Some (parse_staged_spec annotation)
+  | _ -> None
+
+let extract_attribute parser attrs =
+  match List.filter_map parser attrs with
+  | item::_ -> Some item
+  | _ -> None
+
+let extract_spec_attribute attrs = extract_attribute parse_spec_attribute attrs
