2f888c6f15d3bbf5cf3251c0272fda83b320ba3f
Darius Foo <darius.foo.tw@gmail.com>
2025-06-18 16:54:52 +0800

    Update syntax

--- test/examples/closure.ml
+++ test/examples/closure.ml
@@ -1,12 +1,12 @@
 
 let closure_with_effects ()
-(*@ ex i j; ens i->2*j->3/\res=5 @*)
+(*@ ex i j. ens i->2*j->3/\res=5 @*)
 = let i = ref 1 in
   let j = ref 2 in
   let cl x
   (*@
-    ex a b; req x->a*j->b; ens j->b+a * x->a+1 /\ res=b+a+a+1
-    \/ ex a; req x->a /\ x=j; ens j->a+a+1 /\ res=a+a+1+a+1
+    ex a b. req x->a*j->b; ens j->b+a * x->a+1 /\ res=b+a+a+1
+    \/ ex a. req x->a /\ x=j; ens j->a+a+1 /\ res=a+a+1+a+1
   @*)
   = j := !j + !x;
     x := !x + 1;
@@ -15,7 +15,7 @@ let closure_with_effects ()
   cl i
 
 let closures_with_local_state ()
-(*@ ex i j; ens i->1 * j->2/\res=3 @*)
+(*@ ex i j. ens i->1 * j->2/\res=3 @*)
 = let f =
     let x = ref 0 in
     fun () -> x := !x + 1; !x
