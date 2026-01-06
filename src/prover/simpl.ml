6702d0cf39bc7829735548ed3ba9bf3be3a83977
VietAnh1010 <vietanh101003@gmail.com>
2026-01-06 15:37:25 +0800

    Naive implementation of `simpl`, only for sequence and bind.
    At the moment, the simplifer will fail on forall, exists, shift,
    reset, and dollar. The old implementation works, but it is too
    adhoc.

---
+++ src/prover/simpl.ml
@@ -0,0 +1,66 @@
+open Bindlib
+open Core.Syntax
+
+type cont =
+  | CNil
+  | CCons0 of staged_spec * cont
+  | CCons1 of (term, staged_spec) binder * cont
+
+let rec simpl_term (t : term) : term = t
+(*
+  match t with
+  | TVar _ -> t
+  | TUnit -> TUnit
+  | TTrue -> TTrue
+  | TFalse -> TFalse
+  | TInt _ -> t
+  | TTuple ts -> TTuple (List.map simpl_term ts)
+  | TFun b -> TFun b
+      (* let b = simpl_staged_spec_binder b in *)
+      (* TFun b *)
+  | TBinop _ -> t
+  | TUnop _ -> t
+*)
+and simpl_prop (p : prop) : prop = p
+and simpl_hprop (p : hprop) : hprop = p
+
+(** For simplicity, we only simplifies [Sequence] and [Bind] at the moment. *)
+let rec simpl_staged_spec (s : staged_spec) : staged_spec =
+  match s with
+  | Return t -> Return t
+  | Requires p -> Requires p
+  | Ensures p -> Ensures p
+  | Sequence (s1, s2) -> simpl_staged_spec_cont s1 (CCons0 (s2, CNil))
+  | Bind (s, b) -> simpl_staged_spec_cont s (CCons1 (b, CNil))
+  | Apply (f, t) ->
+      begin match f with
+      | TFun b -> simpl_staged_spec (subst b t)
+      | _ -> Apply (f, t)
+      end
+  | Disjunct (s1, s2) -> Disjunct (simpl_staged_spec s1, simpl_staged_spec s2)
+  | Forall b -> Forall b
+  | Exists b -> Exists b
+  | Shift b -> Shift b
+  | Reset s -> simpl_staged_spec_cont s CNil
+  | Dollar (s, k) -> simpl_staged_spec_cont s (CCons1 (k, CNil))
+
+and simpl_staged_spec_cont (s : staged_spec) (k : cont) : staged_spec =
+  match s with
+  | Return t -> invoke_cont k t
+  | Requires p -> Sequence (Requires p, invoke_cont k TUnit)
+  | Ensures p -> Sequence (Ensures p, invoke_cont k TUnit)
+  | Sequence (s1, s2) -> simpl_staged_spec_cont s1 (CCons0 (s2, k))
+  | Bind (s, b) -> simpl_staged_spec_cont s (CCons1 (b, k))
+  | Apply _ -> failwith "todo"
+  | Disjunct (s1, s2) -> Disjunct (simpl_staged_spec_cont s1 k, simpl_staged_spec_cont s2 k)
+  | Forall _ -> failwith "todo"
+  | Exists _ -> failwith "todo"
+  | Shift _ -> failwith "todo"
+  | Reset _ -> failwith "todo"
+  | Dollar _ -> failwith "todo"
+
+and invoke_cont (k : cont) (t : term) =
+  match k with
+  | CNil -> Return t
+  | CCons0 (s, k) -> simpl_staged_spec_cont s k
+  | CCons1 (b, k) -> simpl_staged_spec_cont (subst b t) k
