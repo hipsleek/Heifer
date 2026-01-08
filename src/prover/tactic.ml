7d342cc94544f6d2896fa72a28d4a0443e52236c
Darius Foo <darius.foo.tw@gmail.com>
2026-01-08 12:16:34 +0800

    More correct implementation

--- src/prover/tactic.ml
+++ src/prover/tactic.ml
@@ -9,33 +9,40 @@ module Pctx = struct
   type t = {
     constants : term list;
     assumptions : prop list;
-    goals : sequent list;
+    goal : sequent;
   }
 
-  let create ?(goals = []) () = { constants = []; assumptions = []; goals }
+  let create ~goal () = { constants = []; assumptions = []; goal }
   let draw_line n = String.make n '-'
 
-  let pp ppf { constants; assumptions; goals } =
-    match goals with
+  let pp ppf { constants; assumptions; goal = l, r } =
+    let line = draw_line 20 in
+    Fmt.pf ppf "@[<v>@[<hov>%a@]@,%a%s@,  %a@,⊑ %a@,@]"
+      Fmt.(list ~sep:comma pp_term)
+      constants
+      Fmt.(list ~sep:cut pp_prop)
+      assumptions line pp_staged_spec l pp_staged_spec r
+end
+
+module Pstate = struct
+  type t = Pctx.t list
+
+  let pp ppf s =
+    match s with
     | [] -> Fmt.pf ppf "no more goals"
-    | (l, r) :: goals1 ->
-      let line = draw_line 20 in
+    | p :: goals1 ->
       let goal_text =
         match List.length goals1 with
         | 0 -> ""
         | n -> Format.asprintf "(%d more goals)" n
       in
-      Fmt.pf ppf "@[<v>@[<hov>%a@]@,%a%s@,  %a@,⊑ %a@,%s@,@]"
-        Fmt.(list ~sep:comma pp_term)
-        constants
-        Fmt.(list ~sep:cut pp_prop)
-        assumptions line pp_staged_spec l pp_staged_spec r goal_text
+      Fmt.pf ppf "@[<v>%a%s@]" Pctx.pp p goal_text
 end
 
 module Tactic : sig
   type 'a t
 
-  val run : 'a t -> Pctx.t -> (Pctx.t, string) result
+  val run : 'a t -> Pstate.t -> (Pstate.t, string) result
   val return : 'a -> 'a t
   val bind : 'a t -> ('a -> 'b t) -> 'b t
   val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
@@ -44,14 +51,14 @@ module Tactic : sig
   val get_goal : sequent t
   val get_lhs : staged_spec t
   val get_rhs : staged_spec t
+  val modify_goal : (sequent -> sequent) -> unit t
   val put_lhs : staged_spec -> unit t
   val put_rhs : staged_spec -> unit t
   val put_goal : staged_spec -> staged_spec -> unit t
-  val modify_goal : (sequent -> sequent) -> unit t
   val add_assumption : prop -> unit t
   val add_constant : term -> unit t
 end = struct
-  type 'a t = Pctx.t -> ('a * Pctx.t, string) Result.t
+  type 'a t = Pstate.t -> ('a * Pstate.t, string) Result.t
 
   let run t ps = Result.map snd (t ps)
   let fail s = fun _ -> Error s
@@ -67,10 +74,8 @@ end = struct
   open Pctx
 
   let get_goal =
-    let* pctx = get in
-    match pctx.goals with
-    | [] -> fail "no more goals"
-    | (f1, f2) :: _ -> return (f1, f2)
+    let* ps = get in
+    match ps with [] -> fail "no more goals" | g :: _ -> return g.goal
 
   let get_lhs =
     let* f1, _ = get_goal in
@@ -83,22 +88,26 @@ end = struct
   let put s = fun _ -> Ok ((), s)
 
   let modify_goal f =
-    let* pctx = get in
-    match pctx.goals with
+    let* ps = get in
+    match ps with
     | [] -> fail "no more goals"
-    | g :: gs -> put { pctx with goals = f g :: gs }
+    | g :: gs -> put ({ g with goal = f g.goal } :: gs)
 
   let put_goal l r = modify_goal (fun _ -> (l, r))
   let put_lhs l = modify_goal (fun (_, r) -> (l, r))
   let put_rhs r = modify_goal (fun (l, _) -> (l, r))
 
-  let add_assumption p =
-    let* pctx = get in
-    put Pctx.{ pctx with assumptions = p :: pctx.assumptions }
+  let add_assumption pr =
+    let* ps = get in
+    match ps with
+    | [] -> fail "no more goals"
+    | g :: gs -> put ({ g with assumptions = pr :: g.assumptions } :: gs)
 
   let add_constant t =
-    let* pctx = get in
-    put Pctx.{ pctx with constants = t :: pctx.constants }
+    let* ps = get in
+    match ps with
+    | [] -> fail "no more goals"
+    | g :: gs -> put ({ g with constants = t :: g.constants } :: gs)
 end
 
 let intro =
@@ -124,25 +133,20 @@ let induction wf =
   add_assumption (PImplies (parse_prop wf, PSubsumes (left, right)))
 
 module ProofState = struct
-  let current_state = ref None
-
-  let print_proof_state () =
-    Format.printf "%a@." Pctx.pp (Option.get !current_state)
+  let current_state = ref []
+  let print_proof_state () = Format.printf "%a@." Pstate.pp !current_state
 
   let start_proof l r =
     current_state :=
-      Some (Pctx.create ~goals:[(parse_staged_spec l, parse_staged_spec r)] ());
+      [Pctx.create ~goal:(parse_staged_spec l, parse_staged_spec r) ()];
     print_proof_state ()
 
   let make_interactive (tac : 'b -> 'a Tactic.t) (arg : 'b) =
-    match !current_state with
-    | None -> Format.printf "no goal@."
-    | Some st ->
-      (match Tactic.run (tac arg) st with
-      | Ok next_st ->
-        current_state := Some next_st;
-        print_proof_state ()
-      | Error s -> Format.printf "error: %s@." s)
+    match Tactic.run (tac arg) !current_state with
+    | Ok next_st ->
+      current_state := next_st;
+      print_proof_state ()
+    | Error s -> Format.printf "error: %s@." s
 end
 
 module Interactive = struct
