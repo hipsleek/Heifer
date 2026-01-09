a013258e658b4e97f46ab600ac26519c6005375a
Darius Foo <darius.foo.tw@gmail.com>
2026-01-09 22:53:02 +0800

    Port biabduction

---
+++ src/prover/biab.ml
@@ -0,0 +1,130 @@
+open Core.Syntax
+open Core.Pretty
+
+let rec foldr1_aux f y = function [] -> y | x :: xs -> f y (foldr1_aux f x xs)
+
+let foldr1 ?default f = function
+  | [] -> (match default with None -> failwith "foldr1: empty" | Some a -> a)
+  | x :: xs -> foldr1_aux f x xs
+
+let rec find_delete_map (f : 'a -> 'b option) (xs : 'a list) :
+    ('b * 'a list) option =
+  match xs with
+  | [] -> None
+  | x :: xs ->
+    (match f x with
+    | Some y -> Some (y, xs)
+    | None ->
+      (match find_delete_map f xs with
+      | None -> None
+      | Some (y, xs') -> Some (y, x :: xs')))
+
+(* precondition: all separation conjuctions are flatten into a list of conjucts *)
+let rec match_points_to (ks1 : hprop list) (ks2 : hprop list) :
+    hprop list * hprop list * hprop list * prop list =
+  match ks1 with
+  | [] -> ([], ks2, [], [])
+  | (HPointsTo (x, t) as k) :: ks1 ->
+    (* we try to match on the location *)
+    let match_loc = function
+      | HPointsTo (x', t') when x = x' -> Some t'
+      | _ -> None
+    in
+    begin match find_delete_map match_loc ks2 with
+    | None ->
+      (* the location does not exists in the rhs *)
+      (* therefore we add it into the rhs residue *)
+      let common, anti_frame, frame, equalities = match_points_to ks1 ks2 in
+      (common, anti_frame, k :: frame, equalities)
+    | Some (t', ks2) ->
+      (* generate an equality here *)
+      (* let ctx = if t = t' then ctx else {equalities = eq t t' :: ctx.equalities} in *)
+      let common, anti_frame, frame, equalities = match_points_to ks1 ks2 in
+      let equalities =
+        if t = t' then equalities else PAtom (TBinop (Eq, t, t')) :: equalities
+      in
+      (k :: common, anti_frame, frame, equalities)
+    end
+  | HEmp :: _ | HSepConj _ :: _ -> failwith "match_points_to"
+  | HPure _ :: _ -> failwith "hpure"
+
+let rec conjuncts_of_kappa (k : hprop) : hprop list =
+  match k with
+  | HEmp -> []
+  | HPointsTo _ -> [k]
+  | HSepConj (k1, k2) -> conjuncts_of_kappa k1 @ conjuncts_of_kappa k2
+  | HPure _ -> failwith "hpure"
+
+(* A * H1 |- H2 * D *)
+(* the caller has h1 and h2 and therefore we don't need to return that *)
+(* because we may have many solutions, that's why we need to use Iter.t.
+   but I am not going to use it now *)
+let solve (h1 : hprop) (h2 : hprop) :
+    hprop list * hprop list * hprop list * prop list =
+  let heap1 = conjuncts_of_kappa h1 in
+  let heap2 = conjuncts_of_kappa h2 in
+  (* now match h1 and h2 together. *)
+  (* we need to match heap1 and heap2 together *)
+  (* this is O(n^2) but do we do not care about efficiency atm *)
+  (* if we can sort the conjucts and then do something like "merge"
+     then the complexity of O(n log n) *)
+  match_points_to heap1 heap2
+
+let sep_conj = foldr1 ~default:HEmp (fun c t -> HSepConj (c, t))
+let conj = foldr1 ~default:(PAtom TTrue) (fun c t -> PConj (c, t))
+let fvar x = TSymbol { sym_name = x }
+let points_to l v = HPointsTo (fvar l, v)
+let num n = TInt n
+let plus a b = TApp ("plus", [a; b])
+
+(* see Tests for more e2e tests, which would be nice to port here *)
+let%expect_test _ =
+  let test h1 h2 =
+    let common, anti_frame, frame, equalities = solve h1 h2 in
+    let common = sep_conj common in
+    let anti_frame = sep_conj anti_frame in
+    let frame = sep_conj frame in
+    let equalities = conj equalities in
+    Format.printf "%a * %a |- %a * %a\n%a@." pp_hprop anti_frame pp_hprop common
+      pp_hprop common pp_hprop frame pp_prop equalities
+  in
+  let _ =
+    let h1 = points_to "x" (num 1) in
+    let h2 = points_to "x" (num 1) in
+    test h1 h2;
+    [%expect {|
+      emp * x->1 |- x->1 * emp
+      true
+      |}]
+  in
+  let _ =
+    let h1 = points_to "x" (num 1) in
+    let h2 = points_to "y" (num 2) in
+    test h1 h2;
+    [%expect {|
+      y->2 * emp |- emp * x->1
+      true
+      |}]
+  in
+  let _ =
+    let h1 = points_to "x" (fvar "a") in
+    let h2 = points_to "x" (num 1) in
+    test h1 h2;
+    [%expect {|
+      emp * x->a |- x->a * emp
+      a=1
+      |}]
+  in
+  let _ =
+    let h1 = sep_conj [points_to "v14" (num 0); points_to "v15" (num 2)] in
+    let h2 =
+      sep_conj [points_to "v15" (plus (num 1) (num 1)); points_to "v14" (num 0)]
+    in
+    test h1 h2;
+    [%expect
+      {|
+      emp * v14->0 * v15->2 |- v14->0 * v15->2 * emp
+      2=plus(1, 1)
+      |}]
+  in
+  ()
---
+++ src/prover/biab.mli
@@ -0,0 +1,7 @@
+
+open Core.Syntax
+
+(** [solve h1 h2] solves the biabduction problem {m A * H_1 \vdash H_2 * F}.
+    Returns {m H_1}, {m A}, {m F}, and extra equalities required to make {m H_1 = H_2}.
+*)
+val solve : hprop -> hprop -> hprop list * hprop list * hprop list * prop list
