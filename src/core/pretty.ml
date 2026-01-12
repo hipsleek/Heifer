e5f5c783daed968647d6eff300bbf4860f76fb5f
Darius Foo <darius.foo.tw@gmail.com>
2026-01-12 15:12:44 +0800

    Fix pretty

--- src/core/pretty.ml
+++ src/core/pretty.ml
@@ -36,27 +36,25 @@ let parens_if cond pp ppf x = if cond then Fmt.pf ppf "(%a)" pp x else pp ppf x
     - If a child has lower precedence than the parent context, wrap in parens
     - If left associative, decrement for left child *)
 let rec pp_term_prec prec ctxt ppf = function
-  | TVar x -> Fmt.string ppf (name_of x)
-  | TSymbol sym -> Fmt.string ppf sym.sym_name
-  | TUnit -> Fmt.string ppf "()"
-  | TNil -> Fmt.string ppf "[]"
-  | TTrue -> Fmt.string ppf "true"
-  | TFalse -> Fmt.string ppf "false"
-  | TInt i -> Fmt.int ppf i
-  | TApp (f, xs) ->
-    Fmt.pf ppf "%s$(%a)" f Fmt.(list ~sep:(any ",@ ") (pp_term_prec 0 ctxt)) xs
-  | TFun b ->
+  | Var x -> Fmt.string ppf (name_of x)
+  | Symbol sym -> Fmt.string ppf sym.sym_name
+  | Unit -> Fmt.string ppf "()"
+  | Nil -> Fmt.string ppf "[]"
+  | True -> Fmt.string ppf "true"
+  | False -> Fmt.string ppf "false"
+  | Int i -> Fmt.int ppf i
+  (* | App (f, xs) ->
+    Fmt.pf ppf "%s$(%a)" f Fmt.(list ~sep:(any ",@ ") (pp_term_prec 0 ctxt)) xs *)
+  | Fun b ->
     let x, body, ctxt = unmbind_in ctxt b in
     Fmt.pf ppf "@[<hov 2>(fun %a ->@ %a)@]"
       Fmt.(array ~sep:(any " ") string)
-      (names_of x)
-      (pp_staged_spec_prec 0 ctxt)
-      body
-  | TTuple ts ->
+      (names_of x) (pp_term_prec 0 ctxt) body
+  | Tuple ts ->
     Fmt.pf ppf "@[<hov 2>(%a)@]"
       Fmt.(list ~sep:(any ",@ ") (pp_term_prec 0 ctxt))
       ts
-  | TBinop (op, t1, t2) ->
+  | Binop (op, t1, t2) ->
     let sym, assoc, prec' = OpInfo.binary op in
     let left_prec = if assoc = `Right then prec' else prec' - 1 in
     let right_prec = if assoc = `Left then prec' else prec' - 1 in
@@ -68,82 +66,73 @@ let rec pp_term_prec prec ctxt ppf = function
         t2
     in
     parens_if (prec' <= prec) pp ppf ()
-  | TUnop (op, t) ->
+  | Unop (op, t) ->
     let sym, prec' = OpInfo.unary op in
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%s%a@]" sym (pp_term_prec (prec - 1) ctxt) t
     in
     parens_if (prec' <= prec) pp ppf ()
-
-and pp_prop_prec prec ctxt ppf = function
-  | PAtom t -> pp_term_prec 0 ctxt ppf t
-  | PForall b ->
+  (* | Forall b ->
     let x, body, ctxt = unmbind_in ctxt b in
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>forall %a.@ %a"
         Fmt.(array ~sep:(any " ") string)
         (names_of x)
-        (pp_prop_prec (OpInfo.prec_prop_forall - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_prop_forall - 1) ctxt)
         body
     in
-    pp ppf ()
-  | PImplies (t1, t2) ->
+    pp ppf () *)
+  | Implies (t1, t2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a =>@ %a@]"
-        (pp_prop_prec OpInfo.prec_prop_implies ctxt)
+        (pp_term_prec OpInfo.prec_prop_implies ctxt)
         t1
-        (pp_prop_prec (OpInfo.prec_prop_implies - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_prop_implies - 1) ctxt)
         t2
     in
     parens_if (OpInfo.prec_prop_conj <= prec) pp ppf ()
-  | PConj (t1, t2) ->
+  | Conj (t1, t2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a /\\@ %a@]"
-        (pp_prop_prec (OpInfo.prec_prop_conj - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_prop_conj - 1) ctxt)
         t1
-        (pp_prop_prec OpInfo.prec_prop_conj ctxt)
+        (pp_term_prec OpInfo.prec_prop_conj ctxt)
         t2
     in
     parens_if (OpInfo.prec_prop_conj <= prec) pp ppf ()
-  | PSubsumes (t1, t2) ->
+  | Subsumes (t1, t2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a ⊑@ %a@]"
-        (pp_staged_spec_prec (OpInfo.prec_prop_subsumes - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_prop_subsumes - 1) ctxt)
         t1
-        (pp_staged_spec_prec (OpInfo.prec_prop_subsumes - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_prop_subsumes - 1) ctxt)
         t2
     in
     parens_if (OpInfo.prec_prop_subsumes <= prec) pp ppf ()
-
-and pp_hprop_prec prec ctxt ppf = function
-  | HPure p -> pp_prop_prec 0 ctxt ppf p
-  | HEmp -> Fmt.string ppf "emp"
-  | HPointsTo (t1, t2) ->
+  | Emp -> Fmt.string ppf "emp"
+  | PointsTo (t1, t2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a->%a@]" (pp_term_prec 0 ctxt) t1
         (pp_term_prec 0 ctxt) t2
     in
     parens_if (OpInfo.prec_hprop_pointsto <= prec) pp ppf ()
-  | HSepConj (h1, h2) ->
+  | SepConj (h1, h2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a *@ %a@]"
-        (pp_hprop_prec (OpInfo.prec_hprop_sepconj - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_hprop_sepconj - 1) ctxt)
         h1
-        (pp_hprop_prec (OpInfo.prec_hprop_sepconj - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_hprop_sepconj - 1) ctxt)
         h2
     in
     parens_if (OpInfo.prec_hprop_sepconj <= prec) pp ppf ()
-
-and pp_staged_spec_prec prec ctxt ppf = function
-  | Return t -> Fmt.pf ppf "ret %a" (pp_term_prec 0 ctxt) t
-  | Requires h -> Fmt.pf ppf "req %a" (pp_hprop_prec 0 ctxt) h
-  | Ensures h -> Fmt.pf ppf "ens %a" (pp_hprop_prec 0 ctxt) h
+  | Requires h -> Fmt.pf ppf "req %a" (pp_term_prec 0 ctxt) h
+  | Ensures h -> Fmt.pf ppf "ens %a" (pp_term_prec 0 ctxt) h
   | Sequence (s1, s2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 0>%a;@ %a@]"
-        (pp_staged_spec_prec OpInfo.prec_staged_seq ctxt)
+        (pp_term_prec OpInfo.prec_staged_seq ctxt)
         s1
-        (pp_staged_spec_prec (OpInfo.prec_staged_seq - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_staged_seq - 1) ctxt)
         s2
     in
     parens_if (OpInfo.prec_staged_seq <= prec) pp ppf ()
@@ -155,9 +144,9 @@ and pp_staged_spec_prec prec ctxt ppf = function
     in *)
     let pp ppf () =
       Fmt.pf ppf "@[<hov 2>%a;@ %s.@ %a@]"
-        (pp_staged_spec_prec OpInfo.prec_staged_seq ctxt)
+        (pp_term_prec OpInfo.prec_staged_seq ctxt)
         s (name_of x)
-        (pp_staged_spec_prec (OpInfo.prec_staged_seq - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_staged_seq - 1) ctxt)
         body
     in
     parens_if (OpInfo.prec_staged_seq <= prec) pp ppf ()
@@ -165,12 +154,12 @@ and pp_staged_spec_prec prec ctxt ppf = function
     Fmt.pf ppf "@[<hov 2>%a@ %a@]" (pp_term_prec 0 ctxt) f
       Fmt.(list ~sep:(any "@ ") (pp_term_prec 0 ctxt))
       t
-  | Disjunct (s1, s2) ->
+  | Disj (s1, s2) ->
     let pp ppf () =
       Fmt.pf ppf "@[<hov 0>%a@ \\/@ %a@]"
-        (pp_staged_spec_prec (OpInfo.prec_staged_disj - 1) ctxt)
+        (pp_term_prec (OpInfo.prec_staged_disj - 1) ctxt)
         s1
-        (pp_staged_spec_prec OpInfo.prec_staged_disj ctxt)
+        (pp_term_prec OpInfo.prec_staged_disj ctxt)
         s2
     in
     parens_if (OpInfo.prec_staged_disj <= prec) pp ppf ()
@@ -180,7 +169,7 @@ and pp_staged_spec_prec prec ctxt ppf = function
       Fmt.pf ppf "@[<hov 2>forall %a.@ %a@]"
         Fmt.(array ~sep:(any " ") string)
         (names_of xs)
-        (pp_staged_spec_prec OpInfo.prec_staged_quantifier ctxt)
+        (pp_term_prec OpInfo.prec_staged_quantifier ctxt)
         body
     in
     parens_if (OpInfo.prec_staged_quantifier <= prec) pp ppf ()
@@ -190,22 +179,13 @@ and pp_staged_spec_prec prec ctxt ppf = function
       Fmt.pf ppf "@[<hov 2>ex %a.@ %a@]"
         Fmt.(array ~sep:(any " ") string)
         (names_of xs)
-        (pp_staged_spec_prec OpInfo.prec_staged_quantifier ctxt)
+        (pp_term_prec OpInfo.prec_staged_quantifier ctxt)
         body
     in
     parens_if (OpInfo.prec_staged_quantifier <= prec) pp ppf ()
   | Shift b ->
     let k, body, ctxt = unbind_in ctxt b in
-    Fmt.pf ppf "@[<hov 2>shift %s.@ %a@]" (name_of k)
-      (pp_staged_spec_prec 0 ctxt)
-      body
-  | Reset s ->
-    Fmt.pf ppf "@[<hov 2>reset@ (%a)@]" (pp_staged_spec_prec 0 ctxt) s
-  | Dollar _ -> assert false
+    Fmt.pf ppf "@[<hov 2>shift %s.@ %a@]" (name_of k) (pp_term_prec 0 ctxt) body
+  | Reset s -> Fmt.pf ppf "@[<hov 2>reset@ (%a)@]" (pp_term_prec 0 ctxt) s
 
-let pp_staged_spec ppf s =
-  pp_staged_spec_prec 0 (free_vars (box_staged_spec s)) ppf s
-
-let pp_term ppf t = pp_term_prec 0 (free_vars (box_term t)) ppf t
-let pp_prop ppf p = pp_prop_prec 0 (free_vars (box_prop p)) ppf p
-let pp_hprop ppf h = pp_hprop_prec 0 (free_vars (box_hprop h)) ppf h
+let pp_term ppf s = pp_term_prec 0 (free_vars (box_staged_spec s)) ppf s
--- src/core/syntax_util.ml
+++ src/core/syntax_util.ml
@@ -5,64 +5,45 @@ let open_dfun = function
   | Dfun (sym, def) -> sym, def
 
 let open_psubsumes = function
-  | PSubsumes (s1, s2) -> s1, s2
+  | Subsumes (s1, s2) -> s1, s2
   | _ -> invalid_arg "open_psubsumes: not PSubsumes"
 
-let rec term_has_vars xs t : bool =
+let rec has_vars xs t : bool =
   match t with
-  | TVar x -> TVSet.mem x xs
-  | TSymbol _ -> false
-  | TUnit -> false
-  | TTrue -> false
-  | TFalse -> false
-  | TInt _ -> false
-  | TNil -> false
-  | TFun b -> staged_spec_mbinder_has_vars xs b
-  | TApp (_, ts) -> term_list_has_vars xs ts
-  | TTuple ts -> term_list_has_vars xs ts
-  | TBinop (_, t1, t2) -> term_has_vars xs t1 || term_has_vars xs t2
-  | TUnop (_, t) -> term_has_vars xs t
+  | Var x -> TVSet.mem x xs
+  | Symbol _ -> false
+  | Unit -> false
+  | True -> false
+  | False -> false
+  | Int _ -> false
+  | Nil -> false
+  | Fun b -> mbinder_has_vars xs b
+  | Tuple ts -> term_list_has_vars xs ts
+  | Binop (_, t1, t2) -> has_vars xs t1 || has_vars xs t2
+  | Unop (_, t) -> has_vars xs t
+  | Conj (p1, p2) -> has_vars xs p1 || has_vars xs p2
+  | Implies (p1, p2) -> has_vars xs p1 || has_vars xs p2
+  | Subsumes (s1, s2) -> has_vars xs s1 || has_vars xs s2
+  | Emp -> false
+  | PointsTo (t1, t2) -> has_vars xs t1 || has_vars xs t2
+  | SepConj (p1, p2) -> has_vars xs p1 || has_vars xs p2
+  | Requires p -> has_vars xs p
+  | Ensures p -> has_vars xs p
+  | Sequence (s1, s2) -> has_vars xs s1 || has_vars xs s2
+  | Bind (s, b) -> has_vars xs s || binder_has_vars xs b
+  | Apply (t1, t2) -> has_vars xs t1 || List.exists (has_vars xs) t2
+  | Disj (s1, s2) -> has_vars xs s1 || has_vars xs s2
+  | Forall b -> mbinder_has_vars xs b
+  | Exists b -> mbinder_has_vars xs b
+  | Shift b -> binder_has_vars xs b
+  | Reset s -> has_vars xs s
 
-and term_list_has_vars xs = List.exists (term_has_vars xs)
+and term_list_has_vars xs = List.exists (has_vars xs)
 
-and prop_has_vars xs p =
-  match p with
-  | PAtom t -> term_has_vars xs t
-  | PConj (p1, p2) -> prop_has_vars xs p1 || prop_has_vars xs p2
-  | PImplies (p1, p2) -> prop_has_vars xs p1 || prop_has_vars xs p2
-  | PSubsumes (s1, s2) -> staged_spec_has_vars xs s1 || staged_spec_has_vars xs s2
-  | PForall b -> prop_binder_has_vars xs b
+and binder_has_vars xs b =
+  let _, p = unbind b in
+  has_vars xs p
 
-and prop_binder_has_vars xs b =
-  let _, p = unmbind b in
-  prop_has_vars xs p
-
-and hprop_has_vars xs p =
-  match p with
-  | HPure p -> prop_has_vars xs p
-  | HEmp -> false
-  | HPointsTo (t1, t2) -> term_has_vars xs t1 || term_has_vars xs t2
-  | HSepConj (p1, p2) -> hprop_has_vars xs p1 || hprop_has_vars xs p2
-
-and staged_spec_has_vars xs s =
-  match s with
-  | Return t -> term_has_vars xs t
-  | Requires p -> hprop_has_vars xs p
-  | Ensures p -> hprop_has_vars xs p
-  | Sequence (s1, s2) -> staged_spec_has_vars xs s1 || staged_spec_has_vars xs s2
-  | Bind (s, b) -> staged_spec_has_vars xs s || staged_spec_binder_has_vars xs b
-  | Apply (t1, t2) -> term_has_vars xs t1 || List.exists (term_has_vars xs) t2
-  | Disjunct (s1, s2) -> staged_spec_has_vars xs s1 || staged_spec_has_vars xs s2
-  | Forall b -> staged_spec_mbinder_has_vars xs b
-  | Exists b -> staged_spec_mbinder_has_vars xs b
-  | Shift b -> staged_spec_binder_has_vars xs b
-  | Reset s -> staged_spec_has_vars xs s
-  | Dollar _ -> failwith "todo"
-
-and staged_spec_binder_has_vars xs b =
-  let _, s = unbind b in
-  staged_spec_has_vars xs s
-
-and staged_spec_mbinder_has_vars xs b =
+and mbinder_has_vars xs b =
   let _, s = unmbind b in
-  staged_spec_has_vars xs s
+  has_vars xs s
