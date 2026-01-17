a703196671738a4e0968e5ed08d805c524f202ab
Darius Foo <darius.foo.tw@gmail.com>
2026-01-17 14:58:52 +0800

    Context-dependent parsing

--- src/parse/parse.mli
+++ src/parse/parse.mli
@@ -1,5 +1,9 @@
-val parse_prop : string -> Core.Syntax.prop
-val parse_hprop : string -> Core.Syntax.hprop
-val parse_staged_spec : string -> Core.Syntax.staged_spec
-val parse_term : string -> Core.Syntax.term
-val parse_decl : string -> Core.Syntax.decl
+open Core.Syntax
+open Util.Strings
+open Bindlib
+
+val parse_term : ?ctx:term var SMap.t -> string -> term
+val parse_prop : ?ctx:term var SMap.t -> string -> prop
+val parse_hprop : ?ctx:term var SMap.t -> string -> hprop
+val parse_staged_spec : ?ctx:term var SMap.t -> string -> staged_spec
+val parse_decl : string -> decl
--- src/parse/parser_state.ml
+++ src/parse/parser_state.ml
@@ -1,19 +1,15 @@
 open Core.Syntax
 open Bindlib
+open Util.Strings
 
-(** When parsing input into an AST that uses Bindlib, variables and their
-    binders have to use physically equal vars (created with e.g. [new_tvar]).
+type t = (string, term var) Hashtbl.t
 
-    This module provides a stateful symbol table (mapping string names
-    encountered to Bindlib [var]s) for doing this.
+let unbound_vars : t = Hashtbl.create 10
 
-    - When a binder is encountered, a [new_tvar] is created.
-    - The [var] is used when a variable is parsed.
-    - On the way up past the binder, it is removed.
-    - The [add]/[remove] behaviour of [Hashtbl]s handles shadowing. *)
+let init m =
+  Hashtbl.clear unbound_vars;
+  Hashtbl.add_seq unbound_vars (SMap.to_seq m)
 
-let unbound_vars : (string, term var) Hashtbl.t = Hashtbl.create 10
-let reset_state () = Hashtbl.clear unbound_vars
 let create x = Hashtbl.add unbound_vars x (new_tvar x)
 
 let remove x =
@@ -26,16 +22,8 @@ let remove x =
   r
 
 let remove_all xs = List.map remove xs |> Array.of_list
-let get = Hashtbl.find unbound_vars
-let get_opt = Hashtbl.find_opt unbound_vars
 
-(** Resolve an identifier when parsing:
-    - If the identifier is bound, then it's a variable.
-    - Otherwise, we assume that it is a symbol.
-
-    In the future, we may maintain a symbol table for the parser, if we have
-    multiple kind of symbols. *)
 let resolve_identifier x =
-  match get_opt x with
+  match Hashtbl.find_opt unbound_vars x with
   | None -> Symbol { sym_name = x }
   | Some v -> Var v
