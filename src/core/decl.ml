342078759e746d9185798a4288add2076d306ce9
Darius Foo <darius.foo.tw@gmail.com>
2026-01-21 12:12:10 +0800

    Allow redeclaration, so proof scripts are idempotent

--- src/core/decl.ml
+++ src/core/decl.ml
@@ -6,9 +6,6 @@ type symbol_table = def SymMap.t
 
 let empty_table = SymMap.empty
 
-(** Add a new declaration to a symbol table.
-
-    Raise [Failure] if the symbol already exists. *)
+(** Add a new declaration to a symbol table. *)
 let add_decl sym def (table : symbol_table) : symbol_table =
-  if SymMap.mem sym table then failwith "symbol is already declared";
   SymMap.add sym def table
