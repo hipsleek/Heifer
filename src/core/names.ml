8c02df95c3c25e03b4894dac7147d0a449783586
VietAnh1010 <vietanh101003@gmail.com>
2026-01-27 17:42:03 +0800

    Handling names similar to Rocq?

--- src/core/names.ml
+++ src/core/names.ml
@@ -1,5 +1,7 @@
 (* https://github.com/rocq-prover/rocq/blob/master/engine/nameops.ml *)
 
+open Util
+
 module Subscript = struct
   type t = {
     ss_zero : int;  (** Number of leading zeros of the subscript *)
@@ -36,3 +38,98 @@ module Subscript = struct
     let c = Int.compare s1.ss_subs s2.ss_subs in
     if c = 0 then Int.compare s1.ss_zero s2.ss_zero else c
 end
+
+let cut_ident skip_quote s =
+  let len = String.length s in
+  (* [n'] is the position of the first non nullary digit *)
+  let rec numpart n n' =
+    if n = 0 then
+      (* ident made of _ and digits only [and ' if skip_quote]: don't cut it *)
+      len
+    else
+      let c = s.[n - 1] in
+      if c = '0' && n <> len then numpart (n - 1) n'
+      else if Chars.is_digit c then numpart (n - 1) (n - 1)
+      else if skip_quote && (c = '\'' || c = '_') then numpart (n - 1) (n - 1)
+      else n'
+  in
+  numpart len len
+
+let repr_ident s =
+  let numstart = cut_ident false s in
+  let len = String.length s in
+  if numstart = len then (s, None)
+  else (String.sub s 0 numstart, Some (int_of_string (String.sub s numstart (len - numstart))))
+
+let make_ident s = function
+  | None -> s
+  | Some n ->
+      let c = Strings.get_last s in
+      let n = string_of_int n in
+      if not (Chars.is_digit c) then s ^ n else s ^ "_" ^ n
+
+let increment_subscript s =
+  let len = String.length s in
+  let rec add carrypos =
+    let c = s.[carrypos] in
+    if Chars.is_digit c then
+      if c = '9' then begin
+        assert (carrypos > 0);
+        add (carrypos - 1)
+      end
+      else begin
+        let newid = Bytes.of_string s in
+        Bytes.fill newid (carrypos + 1) (len - carrypos - 1) '0';
+        Bytes.set newid carrypos (Chars.succ c);
+        newid
+      end
+    else begin
+      let newid = Bytes.of_string (s ^ "0") in
+      if carrypos < len - 1 then begin
+        Bytes.fill newid (carrypos + 1) (len - carrypos - 1) '0';
+        Bytes.set newid (carrypos + 1) '1'
+      end;
+      newid
+    end
+  in
+  add (len - 1)
+
+let has_subscript s = Chars.is_digit (Strings.get_last s)
+
+let get_subscript s =
+  let len = String.length s in
+  let rec get_suf pos acc =
+    if pos < 0 then (pos, acc)
+    else
+      let c = s.[pos] in
+      if Chars.is_digit c then get_suf (pos - 1) ((Char.code c - Char.code '0') :: acc)
+      else (pos, acc)
+  in
+  let pos, suf = get_suf (len - 1) [] in
+  if pos = len - 1 then (s, Subscript.zero)
+  else
+    let s = String.sub s 0 (pos + 1) in
+    let rec compute_zeros acc = function
+      | [] -> (acc, [])
+      | 0 :: suf -> compute_zeros (acc + 1) suf
+      | suf -> (acc, suf)
+    in
+    let ss_zero, suf = compute_zeros 0 suf in
+    let ss_subs = List.fold_left (fun acc n -> (10 * acc) + n) 0 suf in
+    (s, { ss_subs; ss_zero })
+
+let add_subscript s ss =
+  if Subscript.equal Subscript.zero ss then s
+  else if ss.Subscript.ss_subs = 0 then
+    let pad = String.make ss.Subscript.ss_zero '0' in
+    Printf.sprintf "%s%s" s pad
+  else
+    let pad = String.make ss.Subscript.ss_zero '0' in
+    let suf = ss.Subscript.ss_subs in
+    Printf.sprintf "%s%s%i" s pad suf
+
+let forget_subscript s =
+  let numstart = cut_ident false s in
+  let newid = Bytes.make (numstart + 1) '0' in
+  String.blit s 0 newid 0 numstart;
+  String.of_bytes newid
--- src/util/strings.ml
+++ src/util/strings.ml
@@ -1,42 +1,9 @@
 module SMap = Map.Make (String)
 
-let code_of_0 = Char.code '0'
-let code_of_9 = Char.code '9'
-let code_of_quote = Char.code '\''
-let code_of_underscore = Char.code '_'
+let get_last s = s.[String.length s - 1]
 
 let draw_line n =
   let unicode = true in
   match unicode with
   | true -> String.concat "" (Lists.make n "─")
   | false -> String.make n '-'
-
-let cut_ident skip_quote s =
-  let slen = String.length s in
-  (* [n'] is the position of the first non nullary digit *)
-  let rec numpart n n' =
-    if n = 0 then
-      (* ident made of _ and digits only [and ' if skip_quote]: don't cut it *)
-      slen
-    else
-      let c = Char.code (String.get s (n - 1)) in
-      if c = code_of_0 && n <> slen then numpart (n - 1) n'
-      else if code_of_0 <= c && c <= code_of_9 then numpart (n - 1) (n - 1)
-      else if skip_quote && (c = code_of_quote || c = code_of_underscore) then
-        numpart (n - 1) (n - 1)
-      else n'
-  in
-  numpart slen slen
-
-let repr_ident s =
-  let numstart = cut_ident false s in
-  let slen = String.length s in
-  if numstart = slen then (s, None)
-  else (String.sub s 0 numstart, Some (int_of_string (String.sub s numstart (slen - numstart))))
-
-let make_ident s = function
-  | None -> s
-  | Some n ->
-      let c = Char.code (String.get s (String.length s - 1)) in
-      let n = string_of_int n in
-      if c < code_of_0 || c > code_of_9 then s ^ n else s ^ "_" ^ n
