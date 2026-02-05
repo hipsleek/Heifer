a07b9648a88eb0ec624042de800835f91974ba5e
VietAnh1010 <vietanh101003@gmail.com>
2026-02-05 22:02:38 +0800

    landin auto

---
+++ test/landin_auto.ml
@@ -0,0 +1,60 @@
+open Heifer.Interactive;;
+
+Options.fail_fast := true;;
+
+declare
+  {|
+    landin_rec f =
+      exists l g.
+        ens l->();
+        ens g=(fun n -> forall h. req l->h; ens l->h; f h n);
+        forall v. req l->v; ens l->g;
+        g
+  |}
+;;
+
+declare
+  {|
+    factf self n =
+      ens n=0; 1 \/
+      ens n>0; self (n-1); r. n*.r
+  |}
+;;
+
+start_proof
+  {|
+    forall n. landin_rec factf; f. f n <: exists l f. ens l->f; fact n
+  |}
+;;
+
+forall_intro ();;
+unfold "landin_rec";;
+goal_is
+  {|
+    (exists l g.
+      ens l->();
+      ens g=(fun n1 -> forall h. req l->h; ens l->h; factf h n1);
+      (forall v. req l->v; ens l->g; g)); f. f n
+    <: exists l f. ens l->f; fact n
+  |}
+;;
+exists_elim ();;
+simpl ();;
+ens_heap_elim ();;
+intro_pure "Hg";;
+forall_elim ["()"];;
+simpl ();;
+req_heap_elim ();;
+ens_heap_elim ();;
+rewrite "Hg";;
+simpl ();;
+forall_elim ["g"];;
+req_heap_elim ();;
+exists_intro ["l"; "g"];;
+induction ~name:"IH" (`Int 0) "n";;
+unfold "factf";;
+rewrite "Hg";;
+simple ();;
+qed ();;
+
+Options.fail_fast := false;;
