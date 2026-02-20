920925b79dfd9f5f5fa2df476bfa5401dc2cbef3
Darius Foo <darius.foo.tw@gmail.com>
2026-02-20 16:05:40 +0800

    Redact info and update dependencies

--- scripts/package-artifact.sh
+++ scripts/package-artifact.sh
@@ -5,12 +5,16 @@ PREBUILD=$(mktemp -d)
 mkdir $PREBUILD/heifer
 BUILD=$PREBUILD/heifer
 
-cp -r src test heifer.why dune-project $BUILD
+cp -r src test heifer.why dune-project heifer.opam $BUILD
 
 # edit files
+sed -i '' 's/.*authors.*/(authors "Anonymous")/' $BUILD/dune-project
+sed -i '' 's/.*maintainers.*/(maintainers "anon@nymo.us")/' $BUILD/dune-project
+sed -i '' 's/.*authors.*/authors: ["Anonymous"]/' $BUILD/heifer.opam
+sed -i '' 's/.*maintainer.*/maintainer: ["anon@nymo.us"]/' $BUILD/heifer.opam
 echo 'The project builds with OCaml 5.3.0.
 Use `opam install --deps-only .` to install opam dependencies.
-Use `dune build src test --display=short` to confirm that they are all installed. This should complete fast.
+Use `dune test --display=short` to confirm that they are all installed. This should complete fast.
 Then use `TEST_ALL=1 dune test --display=short` to run all the tests. This should take a few minutes.
 If all goes well, nothing beyond the compilation commands should be printed.
 
